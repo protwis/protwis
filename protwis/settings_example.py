6f83fb63afad7c376301f27dc5c6f6db8b8be5fd
Vignir Isberg <vigniri@gmail.com>
2015-03-17 20:27:54 +0100

    Updated static path in settings

--- protwis/settings_example.py
+++ protwis/settings_example.py
@@ -99,11 +99,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))
-
-# Templates
-
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Serializer
 
