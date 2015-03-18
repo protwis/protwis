01ffae1eaaa1e9c44c6200e25757562c3f12bdc6
stefan_mordalski <fosfataza@gmail.com>
2015-03-18 19:31:46 +0100

    Fighting with forms and views

--- structural_tools_gpcr/views.py
+++ structural_tools_gpcr/views.py
@@ -1,7 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django import forms
 
-# Create your views here.
+#Class for starting page of generic numbers assignment
+class GenericNumberingStart(TemplateView):
+    template_name = 'upload_file_form'
+    upload_form_data = {
+        "pdb_file": forms.FileField(),
+        }
+    upload_file_form = forms.Form(upload_form_data)
+
+
+class FileUploadForm(Form):
+    
+    pass
 
 
 def UploadFile(request):
+    
     pass
