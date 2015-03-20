e77f656c81bef1b06854d9276c00e9f98faece69
stefan_mordalski <fosfataza@gmail.com>
2015-03-20 15:47:17 +0100

    Step 1 of gn assignment done

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 from django.conf import settings
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='home/index_{}.html'.format(settings.SITE_NAME)), {'site_title' : settings.SITE_TITLE}), 
+    url(r'^$', TemplateView.as_view(template_name='home/index_{}.html'.format(settings.SITE_NAME)), {'site_title' : settings.SITE_TITLE}, name='index'), 
 )
--- structural_tools_gpcr/views.py
+++ structural_tools_gpcr/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django import forms
+import inspect
 
 #Class for starting page of generic numbers assignment
 class GenericNumberingStart(TemplateView):
@@ -22,8 +23,31 @@ class GenericNumberingStart(TemplateView):
     upload_form_data = {
         "pdb_file": forms.FileField(),
         }
-    upload_file_form = forms.Form(upload_form_data)
+    form_code = forms.Form()
+    form_code.fields = upload_form_data
     form_id = 'gn_pdb_file'
     url = ''
-    
+
     #Buttons
+    buttons = {
+        'continue' : {
+            'label' : 'Assign generic numbers',
+            'url' : '/structural_tools_gpcr/gn_results',
+            'color' : 'success',
+            },
+        }
+
+    def get_context_data(self, **kwargs):
+        context = super(GenericNumberingStart, self).get_context_data(**kwargs)
+
+        # get attributes of this class and add them to the context
+        context['form_code'] = str(self.form_code)
+        attributes = inspect.getmembers(self, lambda a:not(inspect.isroutine(a)))
+        for a in attributes:
+            if not(a[0].startswith('__') and a[0].endswith('__')):
+                context[a[0]] = a[1]
+        return context
+
+#Class rendering results from generic numbers assignment
+class GenericNumberingResults(TemplateView):
+    pass    
