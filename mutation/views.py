f518e28f2294facee6b05d0fc21c6b9d71c57a2c
Christian Munk <christian@munk.be>
2015-03-23 16:28:50 +0100

    First implementation of import. Puts in the raw entries.

--- mutation/views.py
+++ mutation/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from mutation.testing import *
+
+from mutation.models import *
+from datetime import datetime
+#env/bin/python3 -m pip install xlrd
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def importmutation(request):
+
+	rows = loaddatafromexcel('/vagrant/shared/protwis/mutation/import.xlsx')
+
+	rows = analyse_rows(rows)
+
+	whattoreturn = []
+
+	for r in rows:
+		raw_id = insert_raw(r)
+
+
+
+	return HttpResponse(raw_id)
