3243dd6b0b874b40086663bbec37cf2448a3cbd4
stefan_mordalski <fosfataza@gmail.com>
2015-04-02 15:25:22 +0200

    restructureing build_structure command

--- build/management/commands/build_structures.py
+++ build/management/commands/build_structures.py
@@ -4,8 +4,7 @@ from django.db import connection
 
 from protein.models import Protein
 from residue.models import Residue
-from publication.models import Publication
-from common.models import WebLink, WebResource
+from common.models import WebLink, WebResource, Publication
 from structure.models import Structure, StructureType
 
 from optparse import make_option
@@ -17,6 +16,31 @@ class Command(BaseCommand):
 
     logger = logging.getLogger(__name__)
     structure_build_data_dir = os.sep.join([settings.STATICFILES_DIRS[0], 'structure_data'])
+
+    csv_fields = {
+        'id' : 0, 
+        'prot_name' : 1, 
+        'class' : 2, 
+        'pdb_code' : 3, 
+        'endogenous_ligand' :4, 
+        'resolution' : 5, 
+        'x-ray_ligand' : 6, 
+        'ligand_function' : 7, 
+        'chain' : 8, 
+        'pubmed_id' : 9, 
+        'date' : 10, 
+        'G_protein' : 11, 
+        'stabilizing_agent' : 12, 
+        'n-term' : 13, 
+        'icl1' : 14, 
+        'ecl1' : 15, 
+        'icl2' : 16, 
+        'ecl2.1' : 17, 
+        'ecl2.2' : 18, 
+        'icl3' : 19, 
+        'ecl3m' : 20, 
+        'c-term' : 21,
+        }
 
     help = 'Creates structures from .csv formatted data strored in the directory {}'.format(structure_build_data_dir)
 
@@ -50,20 +74,20 @@ class Command(BaseCommand):
 
                 for structure in structures:
                     s = Structure() 
-                    s.resolution = structure[2]
-                    s.pdb_publication_date = "{!s}".format(datetime.strptime(structure[6], "%Y-%m-%d %H:%M:%S").date())
-                    s.preferred_chain = structure[7]
+                    s.resolution = structure[self.csv_fields['resolution']]
+                    s.pdb_publication_date = "{!s}".format(datetime.strptime(structure[self.csv_fields['date']], "%Y-%m-%d %H:%M:%S").date())
+                    s.preferred_chain = structure[self.csv_fields['chain']]
                     try:
-                        s.protein = Protein.objects.get(entry_name=structure[0])
+                        s.protein = Protein.objects.get(entry_name=structure[self.csv_fields['pdb_code']])
                     except Protein.DoesNotExist:
-                        print("Failed to save the structure {} Protein {} does not exist.".format(structure[1], structure[0]))
-                        self.logger.error("Failed to save the structure {} Protein {} does not exist.".format(structure[1], structure[0]))
+                        print("Failed to save the structure {} Protein {} does not exist.".format(structure[self.csv_fields['pdb_code']], structure[self.csv_fields['prot_name']]))
+                        self.logger.error("Failed to save the structure {} Protein {} does not exist.".format(structure[self.csv_fields['pdb_code']], structure[self.csv_fields['prot_name']]))
                         continue
                     #We assume that the proper web resources are defined already
                     try:
-                        s.pdb_code = WebLink.objects.get(index=structure[1])
+                        s.pdb_code = WebLink.objects.get(index=structure[self.csv_fields['pdb_code']])
                     except WebLink.DoesNotExist:
-                        code = WebLink(index=structure[1], web_resource = WebResource.objects.get(slug='pdb'))
+                        code = WebLink(index=structure[self.csv_fields['pdb_code']], web_resource = WebResource.objects.get(slug='pdb'))
                         code.save()
                         s.pdb_code = code
                     #FIXME: Temporary, since at first we are just using crystals
@@ -75,16 +99,16 @@ class Command(BaseCommand):
                         xray.save()
                         s.structure_type = xray
                     try:
-                        s.publication = Publication.objects.get(web_link__index=structure[5])
+                        s.publication = Publication.objects.get(web_link__index=structure[self.csv_fields['pubmed_id']])
                     except:
                         p = Publication()
                         try:
-                            p.web_link = WebLink.objects.get(index=structure[5], web_resource__slug='pubmed')
+                            p.web_link = WebLink.objects.get(index=structure[self.csv_fields['pubmed_id']], web_resource__slug='pubmed')
                         except WebLink.DoesNotExist:
-                            code = WebLink(index=structure[5], web_resource = WebResource.objects.get(slug='pubmed'))
+                            code = WebLink(index=structure[self.csv_fields['pubmed_id']], web_resource = WebResource.objects.get(slug='pubmed'))
                             code.save()
                             p.web_link = code
-                        p.update_from_pubmed_data(index=structure[5])
+                        p.update_from_pubmed_data(index=structure[self.csv_fields['pubmed_id']])
                         p.save()
                         s.publication = p
 
@@ -92,14 +116,16 @@ class Command(BaseCommand):
                         s.save()
                     except Exception as e:
                         print(e)
-                        self.logger.error("Failed to save the structure {}".format(structure[1]))
+                        self.logger.error("Failed to save the structure {}".format(structure[self.csv_fields['pdb_code']]))
+
+
     def parse_csv_data(self, file_name):
         print('Reading up the structures from file {}'.format(file_name))
         structures_fh = open(file_name, 'r')
         structures = []
         for line in structures_fh:
             #The fileds in the csv I use are:
-            #prot_name, pdb_code, resolution, endogenous_ligand, crystalized_ligand, pubmed_id, pub_date, preferred_chain
+            #id, prot_name, class, pdb_code, endogenous_ligand, resolution, x-ray_ligand, ligand_function, chain, pubmed_id, date, G_protein, stabilizing_agent, n-term, icl1, ecl1, icl2, ecl2.1, ecl2.2, icl3, ecl3m c-term
             structures.append([x.strip().strip('"') for x in line.split(';')])
         print('Done. {:n} structure records parsed'.format(len(structures)))
         return structures
