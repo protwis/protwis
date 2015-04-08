382c15d278d8eb747682948198b5168bd2acac49
Vignir Isberg <vigniri@gmail.com>
2015-04-08 09:05:10 +0200

    Commented out tastypie because of Django 1.8 problems

--- protwis/settings_example.py
+++ protwis/settings_example.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tastypie',
+    # 'tastypie',
     'common',
     'home',
     'protein',
