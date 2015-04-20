96edacc501f92c036c8d50a3035c4579619924b6
Vignir Isberg <vigniri@gmail.com>
2015-04-20 14:10:10 +0200

    Updated build all script

--- build/management/commands/build_all.py
+++ build/management/commands/build_all.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
 
+import datetime
+
 
 class Command(BaseCommand):
     help = 'Runs all build functions'
@@ -9,11 +11,13 @@ class Command(BaseCommand):
         commands = [
             'build_common',
             'build_proteins',
-            # 'build_residues',
-            # 'build_constructs',
-            # 'build_structures',
+            'build_residues',
+            'build_yaml_from_structure cs.tsv',
+            'build_constructs',
+            'build_structures',
         ]
 
         for c in commands:
-            print('Running ' + c)
+            print('{} Running {}'.format(
+                datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S'), c))
             call_command(c)
