d3a44ec2599c04d284773562896cb757f9fca45f
Christian Munk <christian@munk.be>
2015-04-29 12:03:08 +0200

    Fixed ligand and interaction migrations to reflect migration fix commit by Vignir.

--- interaction/migrations/0003_residuefragmentatom.py
+++ interaction/migrations/0003_residuefragmentatom.py
@@ -7,25 +7,28 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('interaction', '0003_auto_20150416_1612'),
+        ('interaction', '0002_auto_20150428_1149'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='ResidueFragmentAtom',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
-                ('atomnr', models.SmallIntegerField()),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
                 ('atomtype', models.CharField(max_length=20)),
+                ('atomnr', models.SmallIntegerField()),
+                ('atomclass', models.CharField(max_length=20)),
                 ('residuename', models.CharField(max_length=20)),
                 ('chain', models.CharField(max_length=20)),
                 ('residuenr', models.SmallIntegerField()),
                 ('x', models.DecimalField(decimal_places=3, max_digits=6)),
                 ('y', models.DecimalField(decimal_places=3, max_digits=6)),
                 ('z', models.DecimalField(decimal_places=3, max_digits=6)),
-                ('occupancy', models.DecimalField(decimal_places=3, max_digits=6)),
-                ('temperature', models.DecimalField(decimal_places=3, max_digits=6)),
+                ('occupancy', models.DecimalField(decimal_places=2, max_digits=6)),
+                ('temperature', models.DecimalField(decimal_places=2, max_digits=6)),
                 ('element_name', models.CharField(max_length=20)),
+                ('interaction', models.ForeignKey(to='interaction.ResidueFragmentInteraction', null=True)),
+                ('structureligandpair', models.ForeignKey(to='interaction.StructureLigandInteraction')),
             ],
             options={
                 'db_table': 'interaction_residue_fragment_atoms',
