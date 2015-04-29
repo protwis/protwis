7dec8aaad2503e762def1b6aa19e3c066e3c71dc
Christian Munk <christian@munk.be>
2015-04-29 12:36:39 +0200

    Removed dependencies file and rebuilddb.sh file.

--- rebuilddb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-psql -U protwis -h localhost -d protwis -c 'drop schema public cascade; create schema public;'
-python3 manage.py makemigrations
-python3 manage.py migrate
-psql -U protwis -h localhost -o protwis < /home/vagrant/protwis.sql;
