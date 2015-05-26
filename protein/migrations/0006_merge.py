aa1457437f73bd7a5954581a0aca2b969f6efe70
Vignir Isberg <vigniri@gmail.com>
2015-05-26 14:38:33 +0200

    Merged conflicting migrations files

---
+++ protein/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('protein', '0005_auto_20150515_1114'),
+        ('protein', '0005_auto_20150518_1651'),
+    ]
+
+    operations = [
+    ]
