4282d1dde69bda2979a52ec408374fa20516bf55
stefan_mordalski <fosfataza@gmail.com>
2015-06-07 02:35:40 +0200

    Done importing fragments from existing library

--- build/management/commands/build_fragments.py
+++ build/management/commands/build_fragments.py
@@ -5,18 +5,20 @@ from django.db import connection
 from interaction.models import *
 from residue.models import Residue
 from protein.models import Protein
-from structure.models import Structure,Rotamer,Fragment
+from structure.models import Structure,Rotamer,Fragment,PdbData
+from structure.functions import extract_pdb_data
 
 from Bio.PDB import PDBParser
-from optparse import make_option
-import logging, os
+import Bio.PDB.Polypeptide as polypeptide
+
+import logging, os, sys
 
 class Command(BaseCommand):
 
     logger = logging.getLogger(__name__)
     
     #fragments_dir = os.sep.join([settings.DATA_DIR, 'fragment_data'])
-    fragments_dir = 'C:\\Users\clz967\documents\protwis_vagrant\shared\protwis\data\fragments'
+    fragments_dir = 'C:\\Users\\Stefan\\documents\\protwis_vagrant\\shared\\protwis\\data\\fragment_data'
 
     #Interactions definitions to be uploaded to the db
     interactions = {
@@ -26,12 +28,14 @@ class Command(BaseCommand):
         'HBA': 'Hydrogen bond acceptor',
         'HBA-IAN': 'Hurr durr',
         'AFE': 'Aromatic face to edge',
+        'AEF': 'Aromatic edge to face',
         'AFF': 'Aromatic face to face',
         'IAN': 'Hurr durr',
         'ICT': 'Hurr durr',
+        'KPI': 'Hurr durr',
         }
 
-    def handle(self):
+    def handle(self, *args, **options):
         # delete any existing fragment data
         fragments = self.read_fragments_dir()
         for fragment_fname in fragments:
@@ -55,41 +59,45 @@ class Command(BaseCommand):
 
         if len(tokens) > 5:
             if len(tokens) == 7:
-                feature = '_'.join(tokens[5], tokens[6])
+                feature = '_'.join([tokens[5], tokens[6]])
             elif len(tokens) == 6:
-                feature = token[5]
+                feature = tokens[5]
 
         #Checking the if the crystal is in the database
         try:
             s = Structure.objects.get(pdb_code__index=pdb_code)
         except Structure.DoesNotExist:
-            self.logger.warning('Cannot find the structure {} in the database. Skipping the fragment {}'.foramat(pdb_code, fragment_file_name.strip().replace('.pdb', '')))
+            self.logger.warning('Cannot find the structure {} in the database. Skipping the fragment {}'.format(pdb_code, fragment_file_name.strip().replace('.pdb', '')))
             return
 
         #ResidueFragmentInteractionType
         try:
             i = ResidueFragmentInteractionType.objects.get_or_create(slug=feature, name=self.interactions[feature])
-        except ResidueFragmentInteractionType.DoesNotExist:
+        except Exception:
             self.logger.info("Failed to find or create feature {}...".format(feature))
         #Rotamer and Fragment
         try:
-            fragment_struct = PDBParser(PERMISSIVE=True).get_structure('frag', fragment_file_name)[0]
+            fragment_struct = PDBParser(PERMISSIVE=True).get_structure('frag', os.sep.join([self.fragments_dir, fragment_file_name]))[0]
             fragment_pdb_data = ''
+            r = None
             for residue in fragment_struct.get_residues():
                 hetfield, resseq, icode=residue.get_id()
-                if hetfield == '': #Amino acid
+                if hetfield == ' ': #Amino acid
                     try:
-                        r = Residue.objects.get()
-                        rot = Rotamer(residue=r.id, Structure=s.id, pdbdata=extract_pdb_data(residue))
+                        r = Residue.objects.get(sequence_number=int(resseq), amino_acid=polypeptide.three_to_one(residue.resname),protein_conformation=s.protein_conformation)
+                        d, created = PdbData.objects.get_or_create(pdb=extract_pdb_data(residue))
+                        rot = Rotamer(residue=r, structure=s, pdbdata=d)
                         rot.save()
                     except Exception as msg:
-                        self.logger.error('Failed to add rotamer {}:{}{}\n'.format(pdb_code, residue.resnum, resseq, msg))
+                        self.logger.error('Failed to add rotamer {}:{}{}\n'.format(pdb_code, resseq, msg))
                         return
                 else:
                     fragment_pdb_data += extract_pdb_data(residue)
             try:
-                f = Fragment(residue=r.id, ligand='', structure=s.id, pdbdata=fragment_pdb_data)
+                fd,create = PdbData.objects.get_or_create(pdb=fragment_pdb_data)
+                f = Fragment(residue=r, ligand=s.ligands.all()[0], structure=s, pdbdata=fd)
                 f.save()
             except Exception as msg:
-                self.logger.error('Failed to add fragment {}'.format(fragment_file_name))
-
+                self.logger.error('Failed to add fragment {}\n{}'.format(fragment_file_name, msg))
+        except Exception as msg:
+            self.logger.error('Failed to add fragment {} to the db\n{}'.format(fragment_file_name, msg))
