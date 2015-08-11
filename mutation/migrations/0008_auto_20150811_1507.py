8f23be017af140341ed5b03d952db11342a86734
Christian Munk <christian@munk.be>
2015-08-11 15:35:41 +0200

    Migrations for new mutant changes and fix to show new amino_acid field in render.

---
+++ mutation/migrations/0008_auto_20150811_1507.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mutation', '0007_auto_20150811_1503'),
+    ]
+
+    operations = [
+        migrations.AlterModelTable(
+            name='mutation',
+            table='mutation',
+        ),
+    ]
