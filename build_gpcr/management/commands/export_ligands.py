7391ecfff94e7c5c416b5a48d57da0e2db982557
Christian Munk <christian@munk.be>
2015-09-09 13:57:48 +0200

    Export/Import ligands and publications. Import part of build_common.

---
+++ build_gpcr/management/commands/export_ligands.py
@@ -0,0 +1,5 @@
+from build.management.commands.export_ligands import Command as ExportLigands
+
+
+class Command(ExportLigands):
+    pass
--- common/models.py
+++ common/models.py
@@ -5,6 +5,7 @@ from string import Template
 import urllib.request,json
 
 import logging
+import re
 
 
 class WebResource(models.Model):
@@ -113,50 +114,54 @@ class Publication(models.Model):
     def update_from_doi(self, doi):
         logger = logging.getLogger('build')
         #Pubmed by doi
-        try:
-            url = "http://search.crossref.org/dois?q={}".format(doi)
-            pub = json.loads(urllib.request.urlopen(url).read().decode('ascii', "ignore"))
-            self.title = pub[0]['title'].strip()
-            self.year = pub[0]['year'].strip()
-            full_cit = [x.strip() for x in reversed(pub[0]['fullCitation'].split(','))]
-            tmp_authors = []   
-            p = re.compile(r'\d\d\d\d') #FIXME SOMETHING ERRORS HERE
-            item = full_cit.pop()  
-            while item.strip()[0] != "'" and p.match(item.strip()) is None:
-                tmp_authors.append(item.strip())
-                item = full_cit.pop()
-            self.authors = ','.join(tmp_authors)      
-            pages = ''
-            vol = ''
-            issue = ''    
-            for i in full_cit:
-                if '<i>' in i:
-                    try:
-                        self.journal = PublicationJournal.objects.get(name=i.replace('<i>','').replace('</i>',''))
-                    except PublicationJournal.DoesNotExist:
-                        j = PublicationJournal(name=i.replace('<i>','').replace('</i>',''))
-                        j.save()
-                        self.journal = j
-                if i.replace("'",'').strip() == title:
-                    continue
-                if i.startswith('pp.'):
-                    pages = i.replace('pp. ','')
-                if i.startswith('no.'):
-                    issue = '({})'.format(i.replace('no. ',''))
-                if i.startswith('vol.'):
-                    vol = i.replace('vol. ','')
-            self.reference = "{}{}:{}".format(vol,issue,pages)    
+        try: #try entrez first -- faster.
+            Entrez.email = 'info@gpcrdb.org'
+            record = Entrez.read(Entrez.esearch(
+                db='pubmed',
+                retmax=1,
+                term=doi
+                ))
+            self.update_from_pubmed_data(record['IdList'][0])
         except Exception as msg:
             #Crossref doi search
-            logger.error("Publication update on doi error - trying pubmed! DOI: "+doi+" error:" + str(msg) )
+            logger.error("Publication update on doi via entrez error - trying crossref! DOI: "+doi+" error:" + str(msg) )
             try:
-                Entrez.email = 'info@gpcrdb.org'
-                record = Entrez.read(Entrez.esearch(
-                    db='pubmed',
-                    retmax=1,
-                    term=doi
-                    ))
-                self.update_from_pubmed_data(record['IdList'][0])
+                url = "http://search.crossref.org/dois?q={}".format(doi)
+                pub = json.loads(urllib.request.urlopen(url).read().decode('ascii', "ignore"))
+                # print(pub)
+                # print(pub[0])
+                # print(pub[0]['title'])
+                self.title = pub[0]['title'].strip()
+                self.year = pub[0]['year'].strip()
+                full_cit = [x.strip() for x in reversed(pub[0]['fullCitation'].split(','))]
+                tmp_authors = []   
+                p = re.compile(r'\d\d\d\d') #FIXME SOMETHING ERRORS HERE
+                item = full_cit.pop()  
+                while item.strip()[0] != "'" and p.match(item.strip()) is None:
+                    tmp_authors.append(item.strip())
+                    item = full_cit.pop()
+                self.authors = ','.join(tmp_authors)      
+                pages = ''
+                vol = ''
+                issue = ''    
+                for i in full_cit:
+                    if '<i>' in i:
+                        try:
+                            self.journal = PublicationJournal.objects.get(name=i.replace('<i>','').replace('</i>',''))
+                        except PublicationJournal.DoesNotExist:
+                            j = PublicationJournal(name=i.replace('<i>','').replace('</i>',''))
+                            j.save()
+                            self.journal = j
+                    if i.replace("'",'').strip() == self.title:
+                        continue
+                    if i.startswith('pp.'):
+                        pages = i.replace('pp. ','')
+                    if i.startswith('no.'):
+                        issue = '({})'.format(i.replace('no. ',''))
+                    if i.startswith('vol.'):
+                        vol = i.replace('vol. ','')
+                self.reference = "{}{}:{}".format(vol,issue,pages)    
+
 
             except Exception as e:
                 logger.error("Publication update on pubmed error! DOI: "+doi+" error:" + str(e) )
