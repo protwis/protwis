12c83b8c95f07c17e76fd428aad402a4c3ddaca0
Christian Munk <christian@munk.be>
2015-09-11 16:01:56 +0200

    First rough draft of upload pdb, run interactions calc, show results.

--- interaction/forms.py
+++ interaction/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
 class PDBform(forms.Form):
-    pdbname = forms.CharField(max_length=10)
+    pdbname = forms.CharField(max_length=10, required=False)
+    file = forms.FileField(label='Select a file',
+                           help_text='max. 42 megabytes', required=False)
