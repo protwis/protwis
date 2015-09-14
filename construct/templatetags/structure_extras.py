3a7be019f6a3d9cb9271ea2072bb19f0e4089a19
EshitaM <eshita.mutt@psi.ch>
2015-09-14 12:21:56 +0200

    Committing after deleting extra migration files

--- construct/templatetags/structure_extras.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def join_attr(obj_list, attr_name, sep=', '):
-    return sep.join(getattr(i, attr_name) for i in obj_list)
