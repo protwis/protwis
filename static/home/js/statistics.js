ed9855e36b1ebb8a2e082c6f11590fe055c14e41
stefan_mordalski <fosfataza@gmail.com>
2015-09-22 11:33:39 +0200

    statistics page fix

--- static/home/js/statistics.js
+++ static/home/js/statistics.js
@@ -1,31 +1,40 @@
 window.onload = function(){
     $(".Class").click(function () {
-        Clear_all()
+        Clear_phylos()
         $(this).css("fill", '#000000');
         point = $('#' + $(this).attr('id')).find('svg')
         $(point).css("visibility", 'hidden');
         $('#'+$(this).attr('id') + '.container').css("display", '');
     });
+    $(".chart_type").click(function () {
+        Clear_charts()
+        $(this).css("fill", '#000000');
+        point = $('#' + $(this).attr('id')).find('svg')
+        $(point).css("visibility", 'hidden');
+        $('#' + $(this).attr('id') + '.chart_container').css("display", '');
+    });
 
-    function Clear_all(){
+    function Clear_phylos(){
         $('#phylos').find(".Class").each(function (index) {
             $(this).css("fill", '');
         });
         $('#phylos').find(".container").each(function (index) {
             $(this).css("display", 'none');
         });
-        $('#charts').find(".Class").each(function (index) {
+    };
+    function Clear_charts(){
+        $('#charts').find(".chart_type").each(function (index) {
             $(this).css("fill", '');
         });
-        $('#charts').find(".container").each(function (index) {
+        $('#charts').find(".chart_container").each(function (index) {
             $(this).css("display", 'none');
         });
     };
     $(document).ready(function () {
         $('#A.Class').css("fill", '#000000');
         $('#A.container').css("display", '');
-        $('#unique.Class').css("fill", '#000000');
-        $('#unique.container').css("display", '');
+        $('#unique.chart_type').css("fill", '#000000');
+        $('#unique.chart_container').css("display", '');
 
     });
         
