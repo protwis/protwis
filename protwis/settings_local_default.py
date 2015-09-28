d53fa6208f6bd0fe658d27b41d13404664c30803
Vignir Isberg <vigniri@gmail.com>
2015-09-28 15:36:43 +0200

    Added analytics code to base template

--- protwis/settings_local_default.py
+++ protwis/settings_local_default.py
@@ -4,14 +4,14 @@
 # Site specific constants
 SITE_NAME = 'gpcr' # used for site specific files
 SITE_TITLE = 'GPCRdb' # for display in templates
-ANALYTICS_KEY = ''
 DATA_DIR = '/vagrant/data/protwis/' + SITE_NAME
 DEFAULT_NUMBERING_SCHEME = 'gpcrdb'
-DEFAULT_SEQUENCE_BASED_NUMBERING_SCHEME = 'bw'
 DEFAULT_PROTEIN_STATE = 'inactive'
 REFERENCE_POSITIONS = {'TM1': '1x50', 'TM2': '2x50', 'TM3': '3x50', 'TM4': '4x50', 'TM5': '5x50', 'TM6': '6x50',
     'TM7': '7x50', 'H8': '8x50'}
 
+# analytics
+GOOGLE_ANALYTICS_KEY = False
 
 # Database
 
