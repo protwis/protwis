79d298c770df18ec8f64c70e047901add84aa1fe
Vignir Isberg <vigniri@gmail.com>
2015-09-28 15:50:40 +0200

    Cleanup of frontpage data lookup

--- home/views.py
+++ home/views.py
@@ -11,13 +11,18 @@ def index(request):
     # title of the page
     context['site_title'] = settings.SITE_TITLE
 
-    # get news 
+    # analytics
+    context['google_analytics_key'] = settings.GOOGLE_ANALYTICS_KEY
+
+    # get news
     context['news'] = News.objects.order_by('-date').all()[:3]
 
     # get release notes
-    context['release_notes'] = ReleaseNotes.objects.all()[0]
-
-    # stats
-    context['release_statistics'] = ReleaseStatistics.objects.filter(release=context['release_notes'])
+    try:
+        context['release_notes'] = ReleaseNotes.objects.all()[0]
+        context['release_statistics'] = ReleaseStatistics.objects.filter(release=context['release_notes'])
+    except IndexError:
+        context['release_notes'] = ''
+        context['release_statistics'] = []
 
     return render(request, 'home/index_{}.html'.format(settings.SITE_NAME), context)
