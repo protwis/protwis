2856a7ca8a0d36521f60300a16a73dd272eb64e0
Christian Munk <christian@munk.be>
2016-08-04 14:39:39 +0200

    Latest construct views

--- construct/urls.py
+++ construct/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.cache import cache_page
 
 from construct import views
 
 
 urlpatterns = [
-    url(r'^$', views.ConstructBrowser.as_view(), name='browse'),
+    # url(r'^$', cache_page(60*60)(views.ConstructBrowser.as_view()), name='browse'),
+    url(r'^$', views.ConstructBrowser.as_view(), name='browse'), #no cache, for dev
     url(r'^auto_webform/(?P<slug>[-\w]+)/$', views.fetch_pdb_for_webform, name='fetch'),
     url(r'^auto/(?P<slug>[-\w]+)/$', views.fetch_pdb, name='fetch'),
     url(r'^auto_all$', views.fetch_all_pdb, name='fetch'),
