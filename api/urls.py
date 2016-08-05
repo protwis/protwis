20f74827ecb0ecb7a420657035a4face3056e3e4
Alexander Sebastian Hauser <alexander.hauser@sund.ku.dk>
2016-08-05 15:36:07 +0200

    Added drugs API

--- api/urls.py
+++ api/urls.py
@@ -63,6 +63,6 @@ urlpatterns = [
 
     url(r'^species/$', views.SpeciesList.as_view(), name='species-list'),
     url(r'^species/(?P<latin_name>[^/]+)/$', views.SpeciesDetail.as_view(), name='species-detail'),
-
-    url(r'^mutants/(?P<entry_name>[^/].+)/$', views.MutantList.as_view(), name='mutants')
+    url(r'^mutants/(?P<entry_name>[^/].+)/$', views.MutantList.as_view(), name='mutants'),
+    url(r'^drugs/(?P<entry_name>[^/].+)/$', views.DrugList.as_view(), name='drugs')
 ]
