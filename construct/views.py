a465be6a5df49c19f98544e6c50064f577b18556
Christian Munk <christian@munk.be>
2016-08-08 16:17:28 +0200

    first version of align of constructs

--- construct/views.py
+++ construct/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, View
 from django.http import HttpResponse
 from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_exempt
 
 from common.diagrams_gpcr import DrawSnakePlot
 
@@ -13,6 +14,8 @@ from mutation.models import Mutation
 
 from datetime import datetime
 import json
+import copy
+from collections import OrderedDict
 
 
 # Create your views here.
@@ -121,3 +124,201 @@ class ConstructBrowser(TemplateView):
             pass
 
         return context
+
+@csrf_exempt #jquery send post, so no csrf
+def align(request):
+
+    ids = json.loads(request.POST.get('ids'))
+
+    c_ids = []
+    s_ids = []
+    for i in ids:
+        if i.startswith('align'):
+            s_ids.append(i.split('_')[1])
+        else:
+            c_ids.append(i)
+
+    cons = Construct.objects.filter(pk__in=c_ids).prefetch_related(
+                "crystal","mutations","purification","protein__family__parent__parent__parent", "insertions", "modifications", "deletions", "crystallization__chemical_lists",
+                "protein__species","structure__pdb_code","structure__publication__web_link", "contributor")
+    proteins = []
+    constructs = OrderedDict()
+    annotations = {}
+    for c in cons:
+        # print(c)
+        proteins.append(c.protein)
+        constructs[c.name] = c.protein.entry_name
+        annotations[c.name] = c.schematic()['annotations']
+
+    print(annotations)
+
+    if len(s_ids):
+        rs = Residue.objects.filter(protein_conformation__protein__in=proteins, protein_segment__slug__in=s_ids).prefetch_related(
+        'protein_conformation__protein', 'protein_conformation__state', 'protein_segment',
+        'generic_number__scheme', 'display_generic_number__scheme')
+    else:
+        rs = Residue.objects.filter(protein_conformation__protein__in=proteins).prefetch_related(
+        'protein_conformation__protein', 'protein_conformation__state', 'protein_segment',
+        'generic_number__scheme', 'display_generic_number__scheme')
+        
+    print("residues",len(rs))
+
+    distinct_gn = []
+    ordered_gn = OrderedDict()
+    distinct_segments = []
+    overview = OrderedDict()
+    segment_length = OrderedDict()
+
+    protein_lookup = {}
+    print('build stuff')
+
+    segment = ''
+    protein = ''
+
+    track_unaligned = {}
+
+
+
+    #Find all possible generic numbers, to ensure gaps
+    for r in rs.order_by('protein_conformation__id','sequence_number'):
+        if segment!=r.protein_segment.slug or protein!=r.protein_conformation.protein.entry_name:
+            no_encountered_gn = True
+            length = 0
+            length_before = 0
+            length_after = 0
+
+        segment = r.protein_segment.slug
+        protein = r.protein_conformation.protein.entry_name
+
+        if protein not in protein_lookup:
+            protein_lookup[protein] = {}
+            track_unaligned[protein] = {}
+
+        if segment not in track_unaligned[protein]:
+            track_unaligned[protein][segment] = {'before':[],'after':[]}
+
+        if segment not in distinct_segments:
+            distinct_segments.append(segment)
+            overview[segment] = OrderedDict()
+            segment_length[segment] = {'aligned':0, 'before':0,'after':0,'total':0}
+
+
+
+        if r.generic_number:   
+            no_encountered_gn = False
+            gn = r.generic_number.label
+            protein_lookup[protein][gn] = {'aa':r.amino_acid,'pos':r.sequence_number}
+            gn_sort = gn.split('x')[1]
+            gn_sort = float("0."+gn_sort)
+            if gn not in distinct_gn:
+                distinct_gn.append(gn)
+                overview[segment][gn_sort] = [gn,{'aa':'-','pos':''}]
+            length += 1
+        else:
+            if no_encountered_gn:
+                track_unaligned[protein][segment]['before'].append({'aa':r.amino_acid,'pos':r.sequence_number})
+                length_before += 1 
+            else:
+                track_unaligned[protein][segment]['after'].append({'aa':r.amino_acid,'pos':r.sequence_number})
+                length_after += 1 
+
+        if len(overview[segment])>segment_length[segment]['aligned']:
+            segment_length[segment]['aligned'] = len(overview[segment])
+        if length_before>segment_length[segment]['before']:
+            segment_length[segment]['before'] = length_before
+        if length_after>segment_length[segment]['after']:
+            segment_length[segment]['after'] = length_after
+        if segment_length[segment]['aligned']+segment_length[segment]['before']+segment_length[segment]['after']>segment_length[segment]['total']:
+            segment_length[segment]['total'] = segment_length[segment]['aligned']+segment_length[segment]['before']+segment_length[segment]['after']
+
+    # SORT generic residues to ensure correct order
+    gn_list = ""
+    ordered_summary = OrderedDict()
+    for seg,gns in overview.items():
+        ordered_summary[seg] = OrderedDict()
+        #GN LIST
+        gn_list += """<td class="ali-td ali-residue res-color-X">&nbsp;</td>"""
+        if seg!='C-term':
+            for _ in range(segment_length[seg]['before']):
+                gn_list += """<td class="ali-td">&nbsp;</td>"""
+        for gn in sorted(gns):
+            ordered_summary[seg][gns[gn][0]] = {'aa':'-','pos':''}
+            gn_list += """<td class="ali-td-generic-num">{}</td>""".format("x"+gns[gn][0].split("x")[1])
+
+        if seg=='C-term':
+            for _ in range(segment_length[seg]['before']):
+                gn_list += """<td class="ali-td">&nbsp;</td>"""
+
+        for _ in range(segment_length[seg]['after']):
+            gn_list += """<td class="ali-td">&nbsp;</td>"""
+
+    alignment = OrderedDict()
+    alignment_print_sequence = ""
+    for c,p in constructs.items():
+        alignment[c] = copy.deepcopy(ordered_summary)
+        alignment_print_sequence += '<tr>'
+        for seg,gns in alignment[c].items():
+
+            if seg not in track_unaligned[p]:
+                track_unaligned[p][seg] = {'before':[],'after':[]}
+
+            alignment_print_sequence += """<td class="ali-td ali-residue res-color-X">&nbsp;</td>"""
+
+            if seg!='C-term':
+                for _ in range(segment_length[seg]['before']-len(track_unaligned[p][seg]['before'])):
+                    alignment_print_sequence += """<td class="ali-td ali-residue res-color-X">
+                                                    -</td>"""
+
+            for aa in track_unaligned[p][seg]['before']:
+                if aa['pos'] in annotations[c]:
+                    annotation = annotations[c][aa['pos']][0]
+                    annotation_text = "<br>"+annotations[c][aa['pos']][1]
+                else:
+                    annotation = ''
+                    annotation_text = ''
+
+                alignment_print_sequence += """<td class="ali-td ali-residue res-color-{}">
+                                                <div data-toggle="tooltip" data-placement="top" data-html="true"
+                                                title="{}{}{}">{}</div></td>""".format(annotation,aa['aa'],aa['pos'],annotation_text,aa['aa'])
+            for gn, aa in gns.items():
+                if gn in protein_lookup[p]:
+                    aa = protein_lookup[p][gn]
+                    alignment[c][seg][gn] = aa
+
+                if aa['pos'] in annotations[c]:
+                    annotation = annotations[c][aa['pos']][0]
+                    annotation_text = "<br>"+annotations[c][aa['pos']][1]
+                else:
+                    annotation = ''
+                    annotation_text = ''
+                alignment_print_sequence += """<td class="ali-td ali-residue res-color-{}">
+                                                <div data-toggle="tooltip" data-placement="top" data-html="true"
+                                                title="{}{}<br>SCHEME: {}{}">{}</div></td>""".format(annotation,aa['aa'],aa['pos'],gn,annotation_text,aa['aa'])
+
+            for aa in track_unaligned[p][seg]['after']:
+                if aa['pos'] in annotations[c]:
+                    annotation = annotations[c][aa['pos']][0]
+                    annotation_text = "<br>"+annotations[c][aa['pos']][1]
+                else:
+                    annotation = ''
+                    annotation_text = ''
+                alignment_print_sequence += """<td class="ali-td ali-residue res-color-{}">
+                                                <div data-toggle="tooltip" data-placement="top" data-html="true"
+                                                title="{}{}{}">{}</div></td>""".format(annotation,aa['aa'],aa['pos'],annotation_text,aa['aa'])
+
+            for _ in range(segment_length[seg]['after']-len(track_unaligned[p][seg]['after'])):
+                alignment_print_sequence += """<td class="ali-td ali-residue res-color-X">
+                                                -</td>"""
+            if seg=='C-term':
+                for _ in range(segment_length[seg]['before']-len(track_unaligned[p][seg]['before'])):
+                    alignment_print_sequence += """<td class="ali-td ali-residue res-color-X">
+                                                    -</td>"""
+
+        alignment_print_sequence += '</tr>'
+
+    print('done',len(alignment_print_sequence))
+    context = {'constructs': constructs,'alignment_print_sequence': alignment_print_sequence, 'segment_length' : segment_length, 'gn_list' : gn_list} #, 'alignment_print_sequence': alignment_print_sequence
+
+    return render(request,'align.html',context)
+
+
