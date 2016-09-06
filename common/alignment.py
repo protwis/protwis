f468e01131ed2e03dd782fe7b371e5b45d637ccb
Gaspar Pandy <pszgaspar@gmail.com>
2016-09-06 13:10:46 +0200

    fixes, chain break at icl3

--- common/alignment.py
+++ common/alignment.py
@@ -925,9 +925,9 @@ class AlignedReferenceTemplate(Alignment):
                                                        "5D5A","5D5B","3KJ6","3NY8","5DGY","5DHG","5DHH","3NYA","5F8U",
                                                        "3OAX","1F88","1GZM","1HZX","3OE6","1L9H","3OE8","4PXF","2G87",
                                                        "3OE9","2HPY","2I35","3PDS","2I36","4RWA","2I37","3PWH","4RWD",
-                                                       "2J4Y","3PXO","2PED","2R4R","2R4S","2VT4","3V2W","3VGA",
+                                                       "2J4Y","3PXO","2PED","2R4R","2R4S","2VT4","3V2W","3VGA","3OE0",
                                             # temp exclusion
-                                                       "4BVN","4AMJ","5A8E"])#.distinct('protein_conformation__protein__parent')
+                                                       "4BVN"])#,"4AMJ",])#,"5A8E"])#.distinct('protein_conformation__protein__parent')
         
         self.load_proteins(
             [Protein.objects.get(id=target.protein_conformation.protein.parent.id) for target in self.structures_data])
@@ -1026,6 +1026,8 @@ class AlignedReferenceTemplate(Alignment):
                     else:
                         raise Exception()
                 except:
+                    if len(main_temp_seq)==0:
+                        continue
                     if len(ref_seq)==len(main_temp_seq) or self.segment_labels[0] in self.provide_alignment.reference_dict:
                         similarity_table[self.main_template_structure] = self.provide_similarity_table[
                                                                                             self.main_template_structure]
@@ -1033,7 +1035,8 @@ class AlignedReferenceTemplate(Alignment):
                         
                     # Allow for partial main loop template
                     elif list(range(main_temp_seq[0].sequence_number,list(main_temp_seq)[-1].sequence_number+1))!=[i.sequence_number for i in main_temp_seq]:
-                        temp_list.append((struct, len(ref_seq), 0, float(struct.resolution), protein))
+                        if self.reference_protein==self.main_template_protein:
+                            temp_list.append((struct, len(ref_seq), 0, float(struct.resolution), protein))
             else:
                 temp_length, temp_length1, temp_length2 = [],[],[]
                 try:
