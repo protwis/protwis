7253468a4be174ddfe56f546a747e1028d0722c9
Gaspar Pandy <pszgaspar@gmail.com>
2016-09-09 09:00:43 +0200

    fixes

--- common/alignment.py
+++ common/alignment.py
@@ -919,15 +919,18 @@ class AlignedReferenceTemplate(Alignment):
         self.structures_data = Structure.objects.filter(
             state__name__in=self.query_states, protein_conformation__protein__parent__family__parent__parent__parent=
             template_family).order_by('protein_conformation__protein__parent',
-            'resolution').exclude(pdb_code__index__in=["2X72","3ZEV","4U14","2Y01","4A4M","4UG2","4WW3","4BEY","4X1H",
-                                                       "4BEZ","2YCX","4XEE","2YCY","4BUO","4XES","4BV0","4BWB","2ZIY",
-                                                       "4DAJ","3AYM","4ZUD","3AYN","4GBR","3C9L","3C9M","3CAP","3EML",
-                                                       "5D5A","5D5B","3KJ6","3NY8","5DGY","5DHG","5DHH","3NYA","5F8U",
-                                                       "3OAX","1F88","1GZM","1HZX","3OE6","1L9H","3OE8","4PXF","2G87",
-                                                       "3OE9","2HPY","2I35","3PDS","2I36","4RWA","2I37","3PWH","4RWD",
-                                                       "2J4Y","3PXO","2PED","2R4R","2R4S","2VT4","3V2W","3VGA","3OE0",
+            'resolution').exclude(pdb_code__index__in=[
+#                                                       "2X72","3ZEV","4U14","2Y01","4A4M","4UG2","4WW3","4BEY","4X1H",
+#                                                       "4BEZ","2YCX","4XEE","2YCY","4BUO","4XES","4BV0","4BWB","2ZIY",
+#                                                       "4DAJ","3AYM","4ZUD","3AYN","4GBR","3C9L","3C9M","3CAP","3EML",
+#                                                       "5D5A","5D5B","3KJ6","3NY8","5DGY","5DHG","5DHH","3NYA","5F8U",
+#                                                       "3OAX","1F88","1GZM","1HZX","3OE6","1L9H","3OE8","4PXF","2G87",
+#                                                       "3OE9","2HPY","2I35","3PDS","2I36","4RWA","2I37","3PWH","4RWD",
+#                                                       "2J4Y","3PXO","2PED","2R4R","2R4S","2VT4","3V2W","3VGA","3OE0",
+                                                       "2YCX","3KJ6","2R4R","2R4S",
                                             # temp exclusion
-                                                       "4BVN"])#,"4AMJ",])#,"5A8E"])#.distinct('protein_conformation__protein__parent')
+                                                       "4EIY","2YDV","4UG2","4UHR","3EML","3VG9","3QAK","2YDO","3VGA",
+                                                       "3UZA","3PWH","3REY","3UZC"])#.distinct('protein_conformation__protein__parent')
         
         self.load_proteins(
             [Protein.objects.get(id=target.protein_conformation.protein.parent.id) for target in self.structures_data])
@@ -1005,6 +1008,13 @@ class AlignedReferenceTemplate(Alignment):
                 ref_ECL2 = self.ECL2_slicer(ref_seq)
             except:
                 ref_ECL2 = None
+                
+#################### temp force adding prev excluded structs to loop selection
+        import structure.homology_models_tests as tests
+        t = tests.HomologyModelsTests()
+        self.provide_similarity_table = t.force_add_template_to_table(self.provide_similarity_table, self.main_template_structure, ['4EIY'])
+################################################
+                
         for struct, similarity in self.provide_similarity_table.items():
             protein = struct.protein_conformation.protein.parent
             if protein==self.main_template_protein:
--- structure/management/commands/list_new_xtals.py
+++ structure/management/commands/list_new_xtals.py
@@ -36,7 +36,7 @@ class QueryPDB():
     <queryType>org.pdb.query.simple.TreeQuery</queryType>
     <description>TransmembraneTree Search for G Protein-Coupled Receptors (GPCRs)</description>
     <t>19</t>
-    <n>236</n>
+    <n>238</n>
     <nodeDesc>G Protein-Coupled Receptors (GPCRs)</nodeDesc>
 </orgPdbQuery>
     
