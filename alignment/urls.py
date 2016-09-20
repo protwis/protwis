cbba26f47513cc3bcb1d1ec17ad07e39fe6330d2
Christian Munk <christian@munk.be>
2016-09-20 16:12:38 +0200

    django 1.10 fix and caching everywhere :)

--- alignment/urls.py
+++ alignment/urls.py
@@ -1,11 +1,12 @@
 ﻿from django.conf.urls import url
 from django.views.generic import TemplateView
+from django.views.decorators.cache import cache_page
 
 from alignment import views
 
 
 urlpatterns = [
-    url(r'^targetselection', views.TargetSelection.as_view(), name='targetselection'),
+    url(r'^targetselection', cache_page(60*60*24*7)(views.TargetSelection.as_view()), name='targetselection'),
     url(r'^segmentselection', views.SegmentSelection.as_view(), name='segmentselection'),
     url(r'^render/(?P<slug>[^/]+)/$', views.render_family_alignment, name='render-family'),
     url(r'^render', views.render_alignment, name='render'),
