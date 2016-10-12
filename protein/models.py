9dd5f34df5272a74041465c6fc6e62c0c149c4b7
Christian Munk <christian@munk.be>
2016-10-12 09:22:41 +0200

    current version construct tool

--- protein/models.py
+++ protein/models.py
@@ -41,6 +41,10 @@ class Protein(models.Model):
         residuelist = Residue.objects.filter(protein_conformation__protein__entry_name=str(self)).prefetch_related('protein_segment','display_generic_number','generic_number')
         return DrawSnakePlot(residuelist,self.get_protein_class(),str(self))
 
+    def get_snake_plot_no_buttons(self):
+        residuelist = Residue.objects.filter(protein_conformation__protein__entry_name=str(self)).prefetch_related('protein_segment','display_generic_number','generic_number')
+        return DrawSnakePlot(residuelist,self.get_protein_class(),str(self), nobuttons=1)
+
     def get_protein_family(self):
         tmp = self.family
         while tmp.parent.parent.parent is not None:
