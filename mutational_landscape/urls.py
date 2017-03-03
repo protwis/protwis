603799ba599d3c326b913264a5bb6ac157d8c542
Alexander Sebastian Hauser <alexander.hauser@sund.ku.dk>
2017-03-03 15:06:32 +0000

    Updated variant build, created new templates for statistics and browser pages, created a first version of a variant table

--- mutational_landscape/urls.py
+++ mutational_landscape/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 
 from mutational_landscape import views
 
 urlpatterns = [
+    url(r'^$', cache_page(60*60*24*0)(views.TargetSelection.as_view()), name='targetselection'),
+    url(r'^render', views.render_variants, name='render'),
+    url(r'^(?P<download>download)', views.render_variants, name='render'),
+    url(r'^statistics', views.statistics, name='statistics'),
     url(r'^ajax/NaturalMutation/(?P<slug>[-\w]+)/$', views.ajaxNaturalMutation, name='ajaxNaturalMutation'),
     url(r'^ajax/CancerMutation/(?P<slug>[-\w]+)/$', views.ajaxCancerMutation, name='ajaxCancerMutation'),
     url(r'^ajax/DiseaseMutation/(?P<slug>[-\w]+)/$', views.ajaxDiseaseMutation, name='ajaxDiseaseMutation'),
