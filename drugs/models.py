d1344561d9f4512d075a1777c3330793f1423240
Alexander Sebastian Hauser <alexander.hauser@sund.ku.dk>
2017-05-18 12:37:02 +0200

    updated drug browser with new clinical data

--- drugs/models.py
+++ drugs/models.py
@@ -16,14 +16,20 @@ from django.db import models
 class Drugs(models.Model):
     target = models.ManyToManyField('protein.Protein')
     name = models.CharField(max_length=75)
-    
+
     synonym = models.CharField(max_length=300, null=True) # addName as list, make as ManyField
 
     drugtype = models.CharField(max_length=40, null=True)
 
+
     status = models.CharField(max_length=15)
     indication = models.CharField(max_length=150, null=True)
     approval = models.CharField(max_length=5)
+
+    # clinicalstatus = models.CharField(max_length=30, null=True,default=False)
+    # phasedate = models.CharField(max_length=15, null=True,default=True)
+    # phase = models.CharField(max_length=5, null=True,default=True)
+    # moa = models.CharField(max_length=30, null=True,default=True)
 
     externallink = models.CharField(max_length=150, null=True) # Link Framework
 
