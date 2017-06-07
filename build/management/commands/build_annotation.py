8cf636c861f71dd06fb0e807abdca61642b3703b
Christian Munk <christian@munk.be>
2017-06-07 11:55:48 +0200

    Build optimisations

--- build/management/commands/build_annotation.py
+++ build/management/commands/build_annotation.py
@@ -71,6 +71,7 @@ class Command(BaseBuild):
 
     pconfs = ProteinConformation.objects.all().order_by('protein__family')
 
+    pconfs = ProteinConformation.objects.filter(protein__sequence_type__slug='wt').all().order_by('protein__family')
     pw_aln_error = ['celr3_mouse','celr3_human','gpr98_human']
 
     track_rf_annotations = {}
@@ -289,7 +290,7 @@ class Command(BaseBuild):
             else:
                 # print(counter,p)
                 if p.protein.species.common_name != "Human" and entry_name not in proteins:
-                    human_ortholog = Protein.objects.filter(family=p.protein.family, species__common_name='Human')
+                    human_ortholog = Protein.objects.filter(family=p.protein.family, sequence_type__slug='wt', species__common_name='Human')
                     if human_ortholog.exists():
                         human_ortholog = human_ortholog.get()
                         if human_ortholog.entry_name not in proteins:
