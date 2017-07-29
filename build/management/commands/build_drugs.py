9af87d99f05d49019f9201150f612ba1e0807228
Alexander Sebastian Hauser <alexander.hauser@sund.ku.dk>
2017-07-29 12:45:25 +0200

    updated drug aliases names

--- build/management/commands/build_drugs.py
+++ build/management/commands/build_drugs.py
@@ -57,6 +57,10 @@ class Command(BaseCommand):
 
             for index, row in enumerate(data.iterrows()):
                 drugname = data[index:index+1]['Drug Name'].values[0]
+                drugalias_raw = data[index:index+1]['DrugAliases'].values[0]
+                drugalias = ['' if str(drugalias_raw) == 'nan' else ', '+str(drugalias_raw)]
+                drugname = drugname + drugalias[0]
+
                 entry_name = data[index:index+1]['EntryName'].values[0]
 
                 phase = data[index:index+1]['Phase'].values[0]
