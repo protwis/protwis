6a82872c821fb92f48599a343addb2eca7aaeb82
Alexander Sebastian Hauser <alexander.hauser@sund.ku.dk>
2017-08-24 14:11:17 +0200

    updated functional site annotation to snakeplot tooltips

--- mutational_landscape/views.py
+++ mutational_landscape/views.py
@@ -221,6 +221,62 @@ def ajaxNaturalMutation(request, slug, **response_kwargs):
 
     name_of_cache = 'ajaxNaturalMutation_'+slug
 
+    ptms = PTMs.objects.filter(protein__entry_name=slug).prefetch_related('residue')
+    ptms_dict = {}
+
+    for ptm in ptms:
+        ptms_dict[ptm.residue.sequence_number] = ptm.modification
+
+    ## MICROSWITCHES
+    micro_switches_rset = ResiduePositionSet.objects.get(name="Microswitches")
+    ms_label = []
+    for residue in micro_switches_rset.residue_position.all():
+        ms_label.append(residue.label)
+
+    ms_object = Residue.objects.filter(protein_conformation__protein__entry_name=slug, generic_number__label__in=ms_label)
+    ms_sequence_numbers = []
+    for ms in ms_object:
+        ms_sequence_numbers.append(ms.sequence_number)
+
+    ## SODIUM POCKET
+    sodium_pocket_rset = ResiduePositionSet.objects.get(name="Sodium pocket")
+    sp_label = []
+    for residue in sodium_pocket_rset.residue_position.all():
+        sp_label.append(residue.label)
+
+    sp_object = Residue.objects.filter(protein_conformation__protein__entry_name=slug, generic_number__label__in=ms_label)
+    sp_sequence_numbers = []
+    for sp in sp_object:
+        sp_sequence_numbers.append(sp.sequence_number)
+
+    ## G PROTEIN INTERACTION POSITIONS
+    # THIS SHOULD BE CLASS SPECIFIC (different set)
+    rset = ResiduePositionSet.objects.get(name='Signalling protein pocket')
+    gprotein_generic_set = []
+    for residue in rset.residue_position.all():
+        gprotein_generic_set.append(residue.label)
+
+    ### GET LB INTERACTION DATA
+    # get also ortholog proteins, which might have been crystallised to extract
+    # interaction data also from those
+    p = Protein.objects.get(entry_name=slug)
+    orthologs = Protein.objects.filter(family__slug__startswith=p.family.slug, sequence_type__slug='wt')
+
+    interactions = ResidueFragmentInteraction.objects.filter(
+        structure_ligand_pair__structure__protein_conformation__protein__parent__in=orthologs, structure_ligand_pair__annotated=True).exclude(interaction_type__type ='hidden').order_by('rotamer__residue__sequence_number')
+    interaction_data = {}
+    for interaction in interactions:
+        if interaction.rotamer.residue.generic_number:
+            sequence_number = interaction.rotamer.residue.sequence_number
+            # sequence_number = lookup[interaction.rotamer.residue.generic_number.label]
+            label = interaction.rotamer.residue.generic_number.label
+            aa = interaction.rotamer.residue.amino_acid
+            interactiontype = interaction.interaction_type.name
+            if sequence_number not in interaction_data:
+                interaction_data[sequence_number] = []
+            if interactiontype not in interaction_data[sequence_number]:
+                interaction_data[sequence_number].append(interactiontype)
+
     jsondata = cache.get(name_of_cache)
 
     if jsondata == None:
@@ -239,8 +295,28 @@ def ajaxNaturalMutation(request, slug, **response_kwargs):
             else:
                 effect = 'deleterious'
                 color = '#575c9d'
+
+            functional_annotation = ''
+            SN = NM.residue.sequence_number
+            if NM.residue.generic_number:
+                GN = NM.residue.generic_number.label
+            else:
+                GN = ''
+            if SN in sp_sequence_numbers:
+                functional_annotation +=  'SodiumPocket '
+            if SN in ms_sequence_numbers:
+                functional_annotation +=  'MicroSwitch '
+            if SN in ptms_dict:
+                functional_annotation +=  'PTM (' + ptms_dict[SN] + ') '
+            if SN in interaction_data:
+                functional_annotation +=  'LB (' + ', '.join(interaction_data[SN]) + ') '
+            if GN in gprotein_generic_set:
+                functional_annotation +=  'GP (contact) '
+
+            if functional_annotation == '':
+                functional_annotation = '-'
             # account for multiple mutations at this position!
-            jsondata[SN] = [NM.amino_acid, NM.allele_frequency, NM.allele_count, NM.allele_number, NM.number_homozygotes, NM.type, effect, color]
+            jsondata[SN] = [NM.amino_acid, NM.allele_frequency, NM.allele_count, NM.allele_number, NM.number_homozygotes, NM.type, effect, color, functional_annotation]
 
         jsondata = json.dumps(jsondata)
         response_kwargs['content_type'] = 'application/json'
