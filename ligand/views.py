f51f4471af68e82a3892ab7ff576480b7d758dfe
Stefan Mordalski <stefanm@if-pan.krakow.pl>
2017-09-07 09:46:09 +0200

    changes and fixes to ligand browser

--- ligand/views.py
+++ ligand/views.py
@@ -5,28 +5,34 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView, View
 from protein.models import Protein, Species, ProteinFamily
 from ligand.models import Ligand, AssayExperiment, LigandProperities 
-# Create your views here.
 
     
-def LigandBrowser(request):
-    
-    ligands = AssayExperiment.objects.values('protein__entry_name', 'protein__family__name', 'protein__family__parent__name', 'protein__family__parent__parent__name', 'protein__family__parent__parent__parent__name', 'protein__family__slug', 'protein__family__parent__slug', 'protein__family__parent__parent__slug', 'protein__family__parent__parent__parent__slug', 'protein__species__common_name').annotate(num_ligands=Count('ligand', distinct=True))
-    
-    print(ligands)
-    
-    #od = collections.OrderedDict(sorted(ligands.items()))
-    #
-    #context = {'ligands': od}
-    context = {'ligands': ligands}
+class LigandBrowser(TemplateView):
+    """
+    Per target summary of ligands.
+    """
+    template_name = 'ligand_browser.html'
 
-    return render(request, 'assay/ligand_browser.html', context)
+    def get_context_data (self, **kwargs):
 
- 
+        context = super(LigandBrowser, self).get_context_data(**kwargs)
+
+        ligands = AssayExperiment.objects.values(
+            'protein__entry_name', 
+            'protein__family__name',
+            'protein__family__parent__name',
+            'protein__family__parent__parent__name',
+            'protein__family__parent__parent__parent__name',
+            'protein__species__common_name'
+            ).annotate(num_ligands=Count('ligand', distinct=True))
+
+        context['ligands'] = ligands
+
+        return context
 
 
+def LigandDetails(request, ligand__id):
 
-def l_detail(request, ligand__id):
-    print (ligand__id)
     ls = AssayExperiment.objects.filter(ligand__properities__web_links__index=ligand__id)
     ligands = ls.values(
         'protein__entry_name', 'protein__family__name', 'protein__family__parent__name',
@@ -35,11 +41,7 @@ def l_detail(request, ligand__id):
     
     
     ps =ls.values('standard_type', 'standard_value', 'assay_type', 'protein__entry_name','published_value', 'published_type', 'assay_id').order_by('protein__entry_name')#.annotate(Avg('standard_value'))#, distinct=True))
-    #for p in ps:
-    #    #print(p.__dict__)
-    #    print (p)#,p['assay_type'], p['standard_type'], p['standard_value'], p['c'])
-    ##print ('ps{}'.format(ps))
-    #
+
     p_dict = {}
     p_dict = defaultdict(lambda: defaultdict(lambda:defaultdict(list)))
     for p in ps:
@@ -60,7 +62,7 @@ def l_detail(request, ligand__id):
             
     for i in p_dict:
         print(i, p_dict[i])
-    #
+
     av_list = []
     for p in ligands:
         #print (p)
@@ -77,18 +79,11 @@ def l_detail(request, ligand__id):
     
     
     
-    return render(request, 'assay/l_detail.html', context)
+    return render(request, 'ligand_detail.html', context)
 
-    
-    
-def p_details_filtered(request, slug):
-    # copy paste ish
-    
-    # filter
-    pass
 
-def p_detail(request, slug):
-    print (slug)
+def TargetDetails(request, slug):
+
     if slug.count('_') == 0 :
         ps = AssayExperiment.objects.filter(protein__family__parent__parent__parent__slug=slug, ligand__properities__web_links__web_resource__slug = 'chembl_ligand')
     elif slug.count('_') == 1 and len(slug) == 7:
@@ -103,7 +98,7 @@ def p_detail(request, slug):
                    'ligand__id', 'ligand__properities_id', 'ligand__properities__web_links__index',
                    'protein__species__common_name', 'protein__entry_name', 'ligand__properities__mw',
                    'ligand__properities__logp', 'ligand__properities__rotatable_bonds', 'ligand__properities__smiles',
-                   'ligand__properities__hdon', 'ligand__properities__hacc','protein', 'ligand__properities__ligandvendorlink__vendor__name'
+                   'ligand__properities__hdon', 'ligand__properities__hacc','protein'
                    ).annotate(num_targets = Count('protein__id', distinct=True))
 
     if slug.count('_') == 1 and len(slug) == 7:
@@ -133,7 +128,7 @@ def p_detail(request, slug):
     #    
     #            
         
-    return render(request, 'assay/p_detail.html', context)
+    return render(request, 'target_detail.html', context)
 
 
 
