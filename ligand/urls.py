d3f4735c3bc6b0790d469cda91be0ecef748529c
Stefan Mordalski <stefanm@if-pan.krakow.pl>
2017-09-07 09:46:09 +0200

    changes and fixes to ligand browser

--- ligand/urls.py
+++ ligand/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from ligand.views import *
 
 urlpatterns = [
-    url(r'^$', LigandBrowser, name='ligand_browser'),
-    url(r'^p/(?P<slug>[-\w]+)/$',p_detail, name='p_detail'),
-    url(r'^l/(?P<ligand__id>[-\w]+)/$',l_detail, name='l_detail'),
+    url(r'^$', LigandBrowser.as_view(), name='ligand_browser'),
+    url(r'^target/(?P<slug>[-\w]+)/$',TargetDetails, name='ligand_target_detail'),
+    url(r'^(?P<ligand__id>[-\w]+)/$',LigandDetails, name='ligand_detail'),
 ]
