61d1027b5f309f4b3aaa5c5ea63954fd2a096e85
Stefan Mordalski <stefanm@if-pan.krakow.pl>
2017-10-02 01:30:34 +0200

    Purchasability browser.

--- ligand/urls.py
+++ ligand/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^target/compact/(?P<slug>[-\w]+)/$',TargetDetailsCompact, name='ligand_target_detail_compact'),
     url(r'^targets$',TargetDetails, name='ligand_target_detail'),
     url(r'^targets_compact',TargetDetailsCompact, name='ligand_target_detail_compact'),
+    url(r'^targets_purchasable',TargetPurchasabilityDetails, name='ligand_target_detail_purchasable'),
     url(r'^(?P<ligand_id>[-\w]+)/$',LigandDetails, name='ligand_detail'),
     url(r'^statistics', LigandStatistics.as_view(), name='ligand_statistics')
 ]
