66be01239e3723e7c3447923c9f7f071b028d299
Christian Munk <christian@munk.be>
2017-10-23 14:30:19 +0200

    Further API clean-up

--- api/views.py
+++ api/views.py
@@ -36,20 +36,9 @@ from urllib.parse import urljoin
 from rest_framework import renderers, response, schemas
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import response, schemas
-from rest_framework.routers import DefaultRouter
-from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
+from rest_framework_swagger.views import get_swagger_view
 
-# router = DefaultRouter(
-#     schema_title='GPCRdb API',
-#     schema_renderers=[OpenAPIRenderer, SwaggerUIRenderer, renderers.CoreJSONRenderer]
-# )
-router = DefaultRouter()
-
-@api_view()
-@renderer_classes([SwaggerUIRenderer, OpenAPIRenderer, renderers.CoreJSONRenderer])
-def schema_view(request):
-    generator = schemas.SchemaGenerator(title='GPCRdb API')
-    return response.Response(generator.get_schema(request=request))
+schema_view = get_swagger_view(title='GPCRdb API')
 
 class ProteinDetail(generics.RetrieveAPIView):
     """
