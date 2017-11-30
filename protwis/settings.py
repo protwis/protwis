8b9be041607248d5c4add6e4969da4539e618e7e
Christian Munk <christian@munk.be>
2017-11-30 10:01:31 +0100

    Set cookie timeout to 24hrs rather than default 2 weeks. Some data is saved in the session (phylogenetic trees, superimposing models etc) this fill up the DB. This is a fix for that.

--- protwis/settings.py
+++ protwis/settings.py
@@ -97,6 +97,7 @@ MEDIA_ROOT = '/protwis/media/protwis'
 # Serializer
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
+SESSION_COOKIE_AGE = 86400 #Expire cookies and session after 24 hrs
 
 SWAGGER_SETTINGS = {
     'USE_SESSION_AUTH' : False,
