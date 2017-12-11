d618daaa494d00925e62d0c6bc48b52332e3d870
Christian Munk <christian@munk.be>
2017-12-11 09:59:07 +0100

    Update construct data file and first implementation of dynamine annotation

--- protein/models.py
+++ protein/models.py
@@ -1,9 +1,11 @@
-﻿from django.db import models
+﻿from django.utils.text import slugify
+from django.db import models
+from django.core.cache import cache
 from common.diagrams_gpcr import DrawHelixBox, DrawSnakePlot
 from common.diagrams_gprotein import DrawGproteinPlot
 from common.diagrams_arrestin import DrawArrestinPlot
 
-from residue.models import Residue, ResidueNumberingScheme, ResidueGenericNumberEquivalent
+from residue.models import Residue, ResidueNumberingScheme, ResidueGenericNumberEquivalent, ResidueDataType, ResidueDataPoint
 
 class Protein(models.Model):
     parent = models.ForeignKey('self', null=True)
@@ -65,6 +67,62 @@ class Protein(models.Model):
         while tmp.parent.parent.parent is not None:
             tmp = tmp.parent
         return tmp.name
+
+    def get_dynamine_prediction(self):
+        json_api_key = '26cf5c434a171cab9220666030cd981bdbe485a449729a7c8c6272b9'
+        job = {'protocol': '1.0',
+               'json_api_key': json_api_key,
+               'sequences': {self.entry_name: self.sequence},
+               'predictions_only': 'true',
+        }
+        url = 'http://dynamine.ibsquare.be/batch_request'
+        from urllib import request, parse
+        import json,time
+
+        d = cache.get('dynamine_prediction_%s' % (self.entry_name))
+        if d:
+            return d
+        try:
+            data = parse.urlencode({'batch':json.dumps(job)}).encode()
+            req =  request.Request(url, data=data) # this will make the method "POST"
+            resp = json.loads(request.urlopen(req).read().decode('UTF-8'))
+        except Exception as e:
+            resp = {}
+            print('error starting job',e)
+        job_id = resp['job_id']
+        poll = {'protocol': '1.0',
+               'json_api_key': json_api_key,
+               'job_id': job_id
+        }
+        resp['status'] = 'Started'
+        tries=0
+        while resp['status'] != 'completed':
+            time.sleep(2)
+            tries += 1
+            try:
+                data = parse.urlencode({'batch':json.dumps(poll)}).encode()
+                req =  request.Request(url, data=data) # this will make the method "POST"
+                resp = json.loads(request.urlopen(req).read().decode('UTF-8'))
+            except Exception as e:
+                resp = {}
+                print('error polling job',e)
+            if tries>60:
+                break
+
+        cache.set('dynamine_prediction_%s' % (self.entry_name), resp, 60*60*24*7) #7 days
+        return resp
+
+    def save_dynamine_prediction(self):
+        r = self.get_dynamine_prediction()
+        dynamine, created = ResidueDataType.objects.get_or_create(slug=slugify('dynamine'), name='Dynamine Prediction')
+        residues = Residue.objects.filter(protein_conformation__protein=self)
+        if r['status'] == 'completed':
+            predictions = r['results']['predictions'][self.entry_name]
+            for i, p in enumerate(predictions):
+                # fetch residue
+                r = residues.filter(sequence_number=i+1).get()
+                point, created = ResidueDataPoint.objects.get_or_create(data_type=dynamine, residue=r, value=p[1])
+
 
 class ProteinConformation(models.Model):
     protein = models.ForeignKey('Protein')
