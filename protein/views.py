137b8b84e689ff01797803e577e4c872f154be9a
AJK-dev <kooistra.a.j@gmail.com>
2018-07-31 10:44:39 +0200

    Include protein name matching after stripping tags (e.g. S1P1)

--- protein/views.py
+++ protein/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views import generic
 from django.http import HttpResponse
-from django.db.models import Q
+from django.db.models import Q, F, Func, Value
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
 from django.urls import reverse
@@ -151,6 +151,10 @@ def SelectionAutocomplete(request):
         else:
             ps = Protein.objects.filter(Q(name__icontains=q) | Q(entry_name__icontains=q) | Q(family__name__icontains=q),
                 species__common_name='Human', source__name='SWISSPROT').exclude(family__slug__startswith=exclusion_slug)[:10]
+            if ps.count() == 0:
+                # Try matching after stripping html tags
+                ps = Protein.objects.annotate(filtered=Func(F('name'), Value('<[^>]+>'), Value(''), Value('gi'), function='regexp_replace')).filter(Q(filtered__icontains=q), species__common_name='Human', source__name='SWISSPROT')
+
         for p in ps:
             p_json = {}
             p_json['id'] = p.id
