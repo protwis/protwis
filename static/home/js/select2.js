5c4fb49409ec4a3091ba4dac4e6ec50fa7374a37
AJK-dev <>
2018-08-30 14:32:22 +0200

    Added extra check to prevent errors

--- static/home/js/select2.js
+++ static/home/js/select2.js
@@ -238,7 +238,7 @@ the specific language governing permissions and limitations under the Apache Lic
     }
 
     function focus($el) {
-        if ($el[0] === document.activeElement) return;
+        if ($el === null || $el[0] === document.activeElement) return;
 
         /* set the focus in a 0 timeout - that way the focus is set after the processing
             of the current event has finished - which seems like the only reliable way
