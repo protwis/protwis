e4f45f633e9e23e84a24e0810fcc0e97c6205d72
AJK-dev <>
2018-11-16 11:36:46 +0100

    FIX: single group # ranges now start at 1 (was 0), unified nomenclature

--- static/home/js/schematicplot.js
+++ static/home/js/schematicplot.js
@@ -827,7 +827,7 @@ function createSchematicPlot(data, containerSelector, options, data1, data2) {
       '</div>';*/
 
     let legendHtml = '<h4 class="center">Frequency (#PDBs)</h4>'
-          + `<p>Range: <span id="clscg-pdbs-range">0 - ${data.pdbs.length}</span></p>`
+          + `<p>Range: <span id="clscg-pdbs-range">1 - ${data.pdbs.length}</span></p>`
           + '<div class="slider-range" data-text-id="clscg-pdbs-range" id="clscg-pdbs-range-slider"></div>'
           + '<div class="temperature-scale">'
           + '<span class="white-to-red"></span>'
@@ -875,7 +875,7 @@ function createSchematicPlot(data, containerSelector, options, data1, data2) {
     $( function() {
       $(`${containerSelector} #clscg-pdbs-range-slider`).slider({
         range: true,
-        min: 0,
+        min: 1,
         max: `${data.pdbs.length}`,
         step: 1,
         values: [0, `${data.pdbs.length}`],
