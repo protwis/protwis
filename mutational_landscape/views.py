3ca708686c67b44e0d4e6fa1ff957286502e66fb
AJK-dev <>
2018-12-13 15:54:57 +0100

    FIX: caching of the variation browser results

--- mutational_landscape/views.py
+++ mutational_landscape/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.core.cache import cache
+from django.core.cache import caches
+try:
+    cache_variation = caches['variation']
+except:
+    cache_variation = cache
+
 from django.db.models import Count, Min, Sum, Avg, Q
-from django.core.cache import cache
 from django.views.decorators.cache import cache_page
+
+import hashlib
 
 from protein.models import Protein, ProteinConformation, ProteinAlias, ProteinFamily, Gene, ProteinGProtein, ProteinGProteinPair
 from residue.models import Residue, ResiduePositionSet, ResidueSet
@@ -60,7 +67,7 @@ class TargetSelection(AbsTargetSelection):
     }
     default_species = False
 
-@cache_page(60*60*24*21)
+#@cache_page(60*60*24*21)
 def render_variants(request, protein=None, family=None, download=None, receptor_class=None, gn=None, aa=None, **response_kwargs):
 
     simple_selection = request.session.get('selection', False)
@@ -98,138 +105,144 @@ def render_variants(request, protein=None, family=None, download=None, receptor_
                 for fp in family_proteins:
                     proteins.append(fp)
 
+    # Caching results for unique protein sets
+    cache_key = "VARIATION_"+hashlib.md5(str(proteins).encode('utf-8')).hexdigest()
+    if not cache_variation.has_key(cache_key):
+        NMs = NaturalMutations.objects.filter(Q(protein__in=proteins)).prefetch_related('residue__generic_number','residue__display_generic_number','residue__protein_segment','protein')
+        ptms = PTMs.objects.filter(Q(protein__in=proteins)).prefetch_related('residue')
+        ptms_dict = {}
 
-    NMs = NaturalMutations.objects.filter(Q(protein__in=proteins)).prefetch_related('residue__generic_number','residue__display_generic_number','residue__protein_segment','protein')
-    ptms = PTMs.objects.filter(Q(protein__in=proteins)).prefetch_related('residue')
-    ptms_dict = {}
+        ## MICROSWITCHES
+        micro_switches_rset = ResiduePositionSet.objects.get(name="State (micro-)switches")
+        ms_label = []
+        for residue in micro_switches_rset.residue_position.all():
+            ms_label.append(residue.label)
 
-    ## MICROSWITCHES
-    micro_switches_rset = ResiduePositionSet.objects.get(name="State (micro-)switches")
-    ms_label = []
-    for residue in micro_switches_rset.residue_position.all():
-        ms_label.append(residue.label)
+        ms_object = Residue.objects.filter(protein_conformation__protein=proteins[0], generic_number__label__in=ms_label)
+        ms_sequence_numbers = []
+        for ms in ms_object:
+            ms_sequence_numbers.append(ms.sequence_number)
 
-    ms_object = Residue.objects.filter(protein_conformation__protein=proteins[0], generic_number__label__in=ms_label)
-    ms_sequence_numbers = []
-    for ms in ms_object:
-        ms_sequence_numbers.append(ms.sequence_number)
+        ## SODIUM POCKET
+        sodium_pocket_rset = ResiduePositionSet.objects.get(name="Sodium ion pocket")
+        sp_label = []
+        for residue in sodium_pocket_rset.residue_position.all():
+            sp_label.append(residue.label)
 
-    ## SODIUM POCKET
-    sodium_pocket_rset = ResiduePositionSet.objects.get(name="Sodium ion pocket")
-    sp_label = []
-    for residue in sodium_pocket_rset.residue_position.all():
-        sp_label.append(residue.label)
+        sp_object = Residue.objects.filter(protein_conformation__protein=proteins[0], generic_number__label__in=ms_label)
+        sp_sequence_numbers = []
+        for sp in sp_object:
+            sp_sequence_numbers.append(sp.sequence_number)
 
-    sp_object = Residue.objects.filter(protein_conformation__protein=proteins[0], generic_number__label__in=ms_label)
-    sp_sequence_numbers = []
-    for sp in sp_object:
-        sp_sequence_numbers.append(sp.sequence_number)
+        for ptm in ptms:
+            ptms_dict[ptm.residue.sequence_number] = ptm.modification
 
-    for ptm in ptms:
-        ptms_dict[ptm.residue.sequence_number] = ptm.modification
+        ## G PROTEIN INTERACTION POSITIONS
+        # THIS SHOULD BE CLASS SPECIFIC (different set)
+        rset = ResiduePositionSet.objects.get(name='G-protein interface')
+        gprotein_generic_set = []
+        for residue in rset.residue_position.all():
+            gprotein_generic_set.append(residue.label)
 
-    ## G PROTEIN INTERACTION POSITIONS
-    # THIS SHOULD BE CLASS SPECIFIC (different set)
-    rset = ResiduePositionSet.objects.get(name='G-protein interface')
-    gprotein_generic_set = []
-    for residue in rset.residue_position.all():
-        gprotein_generic_set.append(residue.label)
-
-    ### GET LB INTERACTION DATA
-    # get also ortholog proteins, which might have been crystallised to extract
-    # interaction data also from those
-    if protein:
-        orthologs = Protein.objects.filter(family__slug=proteins[0].family.slug, sequence_type__slug='wt')
-    else:
-        orthologs = Protein.objects.filter(family__slug__startswith=proteins[0].family.slug, sequence_type__slug='wt')
-
-    interactions = ResidueFragmentInteraction.objects.filter(
-        structure_ligand_pair__structure__protein_conformation__protein__parent__in=orthologs, structure_ligand_pair__annotated=True).exclude(interaction_type__type ='hidden').all()
-    interaction_data = {}
-    for interaction in interactions:
-        if interaction.rotamer.residue.generic_number:
-            sequence_number = interaction.rotamer.residue.sequence_number
-            # sequence_number = lookup[interaction.rotamer.residue.generic_number.label]
-            label = interaction.rotamer.residue.generic_number.label
-            aa = interaction.rotamer.residue.amino_acid
-            interactiontype = interaction.interaction_type.name
-            if sequence_number not in interaction_data:
-                interaction_data[sequence_number] = []
-            if interactiontype not in interaction_data[sequence_number]:
-                interaction_data[sequence_number].append(interactiontype)
-
-    # Fixes fatal error - in case of receptor family selection (e.g. H1 receptors)
-    if target_type == 'family' and len(proteins[0].family.slug) < 15:
-        pc = ProteinConformation.objects.get(protein__family__name=familyname, protein__sequence_type__slug='consensus')
-        residuelist = Residue.objects.filter(protein_conformation=pc).order_by('sequence_number').prefetch_related('protein_segment', 'generic_number', 'display_generic_number')
-    else:
-        residuelist = Residue.objects.filter(protein_conformation__protein=proteins[0]).prefetch_related('protein_segment', 'display_generic_number', 'generic_number')
-
-    jsondata = {}
-    for NM in NMs:
-        functional_annotation = ''
-        SN = NM.residue.sequence_number
-        if NM.residue.generic_number:
-            GN = NM.residue.generic_number.label
+        ### GET LB INTERACTION DATA
+        # get also ortholog proteins, which might have been crystallised to extract
+        # interaction data also from those
+        if protein:
+            orthologs = Protein.objects.filter(family__slug=proteins[0].family.slug, sequence_type__slug='wt')
         else:
-            GN = ''
-        if SN in sp_sequence_numbers:
-            functional_annotation += 'SodiumPocket '
-        if SN in ms_sequence_numbers:
-            functional_annotation += 'MicroSwitch '
-        if SN in ptms_dict:
-            functional_annotation += 'PTM (' + ptms_dict[SN] + ') '
-        if SN in interaction_data:
-            functional_annotation += 'LB (' + ', '.join(interaction_data[SN]) + ') '
-        if GN in gprotein_generic_set:
-            functional_annotation += 'GP (contact) '
+            orthologs = Protein.objects.filter(family__slug__startswith=proteins[0].family.slug, sequence_type__slug='wt')
 
-        ms_type = NM.type
-        if ms_type == 'missense':
-            effect = 'deleterious' if NM.sift_score <= 0.05 or NM.polyphen_score >= 0.1 else 'tolerated'
-            color = '#e30e0e' if NM.sift_score <= 0.05 or NM.polyphen_score >= 0.1 else '#70c070'
+        interactions = ResidueFragmentInteraction.objects.filter(
+            structure_ligand_pair__structure__protein_conformation__protein__parent__in=orthologs, structure_ligand_pair__annotated=True).exclude(interaction_type__type ='hidden').all()
+        interaction_data = {}
+        for interaction in interactions:
+            if interaction.rotamer.residue.generic_number:
+                sequence_number = interaction.rotamer.residue.sequence_number
+                # sequence_number = lookup[interaction.rotamer.residue.generic_number.label]
+                label = interaction.rotamer.residue.generic_number.label
+                aa = interaction.rotamer.residue.amino_acid
+                interactiontype = interaction.interaction_type.name
+                if sequence_number not in interaction_data:
+                    interaction_data[sequence_number] = []
+                if interactiontype not in interaction_data[sequence_number]:
+                    interaction_data[sequence_number].append(interactiontype)
+
+        # Fixes fatal error - in case of receptor family selection (e.g. H1 receptors)
+        if target_type == 'family' and len(proteins[0].family.slug) < 15:
+            pc = ProteinConformation.objects.get(protein__family__name=familyname, protein__sequence_type__slug='consensus')
+            residuelist = Residue.objects.filter(protein_conformation=pc).order_by('sequence_number').prefetch_related('protein_segment', 'generic_number', 'display_generic_number')
         else:
-            effect = 'deleterious'
-            color = '#575c9d'
-        # account for multiple mutations at this position!
-        NM.functional_annotation = functional_annotation
-        # print(NM.functional_annotation)
-        jsondata[SN] = [NM.amino_acid, NM.allele_frequency, NM.allele_count, NM.allele_number, NM.number_homozygotes, NM.type, effect, color, functional_annotation]
+            residuelist = Residue.objects.filter(protein_conformation__protein=proteins[0]).prefetch_related('protein_segment', 'display_generic_number', 'generic_number')
 
-
-    natural_mutation_list = {}
-    max_snp_pos = 1
-    for NM in NMs:
-        if NM.residue.generic_number:
-            if NM.residue.generic_number.label in natural_mutation_list:
-                natural_mutation_list[NM.residue.generic_number.label]['val'] += 1
-                if not str(NM.amino_acid) in natural_mutation_list[NM.residue.generic_number.label]['AA']:
-                    natural_mutation_list[NM.residue.generic_number.label]['AA'] = natural_mutation_list[NM.residue.generic_number.label]['AA'] + str(NM.amino_acid) + ' '
-
-                if natural_mutation_list[NM.residue.generic_number.label]['val'] > max_snp_pos:
-                    max_snp_pos = natural_mutation_list[NM.residue.generic_number.label]['val']
+        jsondata = {}
+        for NM in NMs:
+            functional_annotation = ''
+            SN = NM.residue.sequence_number
+            if NM.residue.generic_number:
+                GN = NM.residue.generic_number.label
             else:
-                natural_mutation_list[NM.residue.generic_number.label] = {'val':1, 'AA': NM.amino_acid + ' '}
+                GN = ''
+            if SN in sp_sequence_numbers:
+                functional_annotation += 'SodiumPocket '
+            if SN in ms_sequence_numbers:
+                functional_annotation += 'MicroSwitch '
+            if SN in ptms_dict:
+                functional_annotation += 'PTM (' + ptms_dict[SN] + ') '
+            if SN in interaction_data:
+                functional_annotation += 'LB (' + ', '.join(interaction_data[SN]) + ') '
+            if GN in gprotein_generic_set:
+                functional_annotation += 'GP (contact) '
 
-    jsondata_natural_mutations = {}
+            ms_type = NM.type
+            if ms_type == 'missense':
+                effect = 'deleterious' if NM.sift_score <= 0.05 or NM.polyphen_score >= 0.1 else 'tolerated'
+                color = '#e30e0e' if NM.sift_score <= 0.05 or NM.polyphen_score >= 0.1 else '#70c070'
+            else:
+                effect = 'deleterious'
+                color = '#575c9d'
+            # account for multiple mutations at this position!
+            NM.functional_annotation = functional_annotation
+            # print(NM.functional_annotation)
+            jsondata[SN] = [NM.amino_acid, NM.allele_frequency, NM.allele_count, NM.allele_number, NM.number_homozygotes, NM.type, effect, color, functional_annotation]
 
-    for r in residuelist:
-        if r.generic_number:
-            if r.generic_number.label in natural_mutation_list:
-                jsondata_natural_mutations[r.sequence_number] = natural_mutation_list[r.generic_number.label]
 
-    jsondata_natural_mutations['color'] = linear_gradient(start_hex="#c79494", finish_hex="#c40100", n=max_snp_pos)
-    # jsondata_cancer_mutations['color'] = linear_gradient(start_hex="#d8baff", finish_hex="#422d65", n=max_cancer_pos)
-    # jsondata_disease_mutations['color'] = linear_gradient(start_hex="#ffa1b1", finish_hex="#6e000b", n=max_disease_pos)
-    #
-    SnakePlot = DrawSnakePlot(residuelist, "Class A", protein, nobuttons=1)
-    HelixBox = DrawHelixBox(residuelist, 'Class A', protein, nobuttons=1)
+        natural_mutation_list = {}
+        max_snp_pos = 1
+        for NM in NMs:
+            if NM.residue.generic_number:
+                if NM.residue.generic_number.label in natural_mutation_list:
+                    natural_mutation_list[NM.residue.generic_number.label]['val'] += 1
+                    if not str(NM.amino_acid) in natural_mutation_list[NM.residue.generic_number.label]['AA']:
+                        natural_mutation_list[NM.residue.generic_number.label]['AA'] = natural_mutation_list[NM.residue.generic_number.label]['AA'] + str(NM.amino_acid) + ' '
+
+                    if natural_mutation_list[NM.residue.generic_number.label]['val'] > max_snp_pos:
+                        max_snp_pos = natural_mutation_list[NM.residue.generic_number.label]['val']
+                else:
+                    natural_mutation_list[NM.residue.generic_number.label] = {'val':1, 'AA': NM.amino_acid + ' '}
+
+        jsondata_natural_mutations = {}
+
+        for r in residuelist:
+            if r.generic_number:
+                if r.generic_number.label in natural_mutation_list:
+                    jsondata_natural_mutations[r.sequence_number] = natural_mutation_list[r.generic_number.label]
+
+        jsondata_natural_mutations['color'] = linear_gradient(start_hex="#c79494", finish_hex="#c40100", n=max_snp_pos)
+        # jsondata_cancer_mutations['color'] = linear_gradient(start_hex="#d8baff", finish_hex="#422d65", n=max_cancer_pos)
+        # jsondata_disease_mutations['color'] = linear_gradient(start_hex="#ffa1b1", finish_hex="#6e000b", n=max_disease_pos)
+        #
+        SnakePlot = DrawSnakePlot(residuelist, "Class A", protein, nobuttons=1)
+        HelixBox = DrawHelixBox(residuelist, 'Class A', protein, nobuttons=1)
+
+        cache_data = {'mutations': NMs, 'type': target_type, 'HelixBox': HelixBox, 'SnakePlot': SnakePlot, 'receptor': str(proteins[0].entry_name), 'mutations_pos_list': json.dumps(jsondata), 'natural_mutations_pos_list': json.dumps(jsondata_natural_mutations)}
+        cache_variation.set(cache_key, cache_data, 60*60*24*21)
+    else:
+        cache_data = cache_variation.get(cache_key)
 
     # EXCEL TABLE EXPORT
     if download:
-
         data = []
-        for r in NMs:
+        for r in cache_data['mutations']:
             values = r.__dict__
             complete = {'protein': r.protein.name, 'sequence_number': r.residue.sequence_number, 'orig_amino_acid': r.residue.amino_acid}
             complete.update(values)
@@ -263,7 +276,7 @@ def render_variants(request, protein=None, family=None, download=None, receptor_
             response['Content-Disposition'] = 'attachment; filename=GPCRdb_' + proteins[0].entry_name + '_variant_data.xlsx'  # % 'mutations'
         return response
 
-    return render(request, 'browser.html', {'mutations': NMs, 'type': target_type, 'HelixBox': HelixBox, 'SnakePlot': SnakePlot, 'receptor': str(proteins[0].entry_name), 'mutations_pos_list': json.dumps(jsondata), 'natural_mutations_pos_list': json.dumps(jsondata_natural_mutations)})
+    return render(request, 'browser.html', cache_data)
 
 def ajaxNaturalMutation(request, slug, **response_kwargs):
 
