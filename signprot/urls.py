f052e8907fea7e7564a4a0723bb46d413681e854
Steven Pilger <stevenpilger@fastmail.com>
2019-03-07 10:13:03 +0100

    added optional trailing slash to matrix url path

--- signprot/urls.py
+++ signprot/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
     url(r'^ginterface[/]?$', views.TargetSelection.as_view(), name='targetselection'),
     url(r'^ajax/barcode/(?P<slug>[-\w]+)/(?P<cutoff>\d+\.\d{0,2})/$', views.ajaxBarcode, name='ajaxBarcode'),
     url(r'^ajax/interface/(?P<slug>[-\w]+)/$', views.ajaxInterface, name='ajaxInterface'),
-    url(r'^(?P<slug>[-\w]+)/$', views.signprotdetail, name='signprotdetail'),
     url(r'^structure/(?P<pdbname>[-\w]+)/$', views.StructureInfo, name='StructureInfo'),
     url(r'^family/(?P<slug>[-\w]+)/$', views.familyDetail, name='familyDetail'),
-    url(r'^matrix/home/$', views.InteractionMatrix, name='InteractionMatrix'),
+    url(r'^matrix[/]?$', views.InteractionMatrix, name='InteractionMatrix'),
     url(r'^matrix/seqsig/', views.IMSequenceSignature, name='SequenceSignature'),
     url(r'^matrix/sigmat/', views.IMSignatureMatch, name='SignatureMatch'),
+    url(r'^(?P<slug>[-\w]+)/$', views.signprotdetail, name='signprotdetail'),
 ]
