e3651e448df7d897552b92421cc64282962bc6fb
AJK-dev <>
2019-03-14 14:22:51 +0100

    CLEANUP: step 1 of cleaning angles

--- angles/models.py
+++ angles/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
-class Angle(models.Model):
-    residue   = models.ForeignKey('residue.Residue', on_delete=models.CASCADE)
-    structure = models.ForeignKey('structure.Structure', on_delete=models.CASCADE)
-    angle    = models.FloatField()
-    b_angle  = models.FloatField()
-    diff_med = models.FloatField()
-    sign_med = models.FloatField(default=0)
-    hse      = models.IntegerField(default=0)
-    sasa     = models.FloatField(default=0)
+class ResidueAngle(models.Model):
+    residue     = models.ForeignKey('residue.Residue', on_delete=models.CASCADE)
+    structure   = models.ForeignKey('structure.Structure', on_delete=models.CASCADE)
+    angle       = models.FloatField()
+    b_angle     = models.FloatField()
+    hse         = models.IntegerField(default=0)
+    sasa        = models.FloatField(default=0)
+    phi         = models.FloatField(default=0)
+    psi         = models.FloatField(default=0)
+    theta       = models.FloatField(default=0)
+    tau         = models.FloatField(default=0)
 
-    def __str__(self):
-        return "angle"
-
-    class Meta:
+    class Meta():
+        db_table = 'residue_angles'
         unique_together = ("residue", "structure")
--- angles/urls.py
+++ angles/urls.py
@@ -4,10 +4,7 @@ from django.conf import settings
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    url(r'^ttest$', views.testTemplate.as_view(), name='xxx'),
-    url(r'^browsertest$', views.browsertest, name='browsertest'),
+    url(r'^angleanalysis$', views.angleanalysis, name='angleanalysis'),
     url(r'^angledat$', views.get_angles, name='anglejson'),
-    url(r'^pdb/(?P<pdbname>\w+)$', views.ServePDB, name='serve_pdb'),
     url(r'^pdbtabledata$', views.PdbTableData, name='pdbtreedata'),
-    url(r'^pdbtabledata2$', views.PdbTableData2, name='pdbtreedata2'),
 ]
