b752a0dcc667a2492fc709fce3baaa55bb6d6ee3
AJK-dev <>
2019-04-05 11:18:46 +0200

    UPDATE: innernode gradients based on SI

--- static/home/js/clusteringtree.js
+++ static/home/js/clusteringtree.js
@@ -130,7 +130,7 @@ function renderTree(data) {
     // Add terminal node with coloring
     vis.selectAll("g.terminal-node").append("circle")
         .attr("r", 5)
-        .style("fill", function(n){
+        .style("fill", function(n) {
           // color based on activity
           switch(annotations[n.name][0]){
               case "active":
@@ -172,6 +172,7 @@ function renderTree(data) {
     vis.selectAll('g.inner.node')
         .append("circle")
         .attr("r", 5)
+        .style("fill", function(n){ if (isNaN(n.score)) return "#FFFFFF"; var score = n.score; if (score>1) score=1; if (score>0){ return shadeColor2("#AAAAAA", 100-(score*70-30))} else { return "#FFAAAA"} })
         .attr('data-length', function(n){ return Math.round(n.y*10) })
         .attr('data-score', function(n){ return n.score })
         .on("mouseover", function(d,i) {
@@ -378,6 +379,26 @@ function renderTree(data) {
         R = parseInt(R + (255-R)*percent/100);
         G = parseInt(G + (255-R)*percent/100);
         B = parseInt(B + (255-R)*percent/100);
+
+        var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+        var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+        var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+        return "#"+RR+GG+BB;
+    }
+
+    // Based on https://stackoverflow.com/questions/5560248
+    function shadeColor2(color, percent) {
+        var R = parseInt(color.substring(1,3),16);
+        var G = parseInt(color.substring(3,5),16);
+        var B = parseInt(color.substring(5,7),16);
+
+        R = parseInt(R * percent/100);
+        G = parseInt(G * percent/100);
+        B = parseInt(B * percent/100);
+        R = (R<255)?R:255;
+        G = (G<255)?G:255;
+        B = (B<255)?B:255;
 
         var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
         var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
