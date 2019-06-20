048ffb08251a43fab7bb39fe2dbf7b0bf172a4a2
Steven Pilger <stevenpilger@fastmail.com>
2019-06-20 10:42:59 +0200

    added crude export to excel button

--- static/home/js/matrix_utilities.js
+++ static/home/js/matrix_utilities.js
@@ -287,17 +287,6 @@ const run_sig_match = function(){
         },
         paging: false,
         buttons: [
-          // {
-          //   text: 'Select all',
-          //   action: function () {
-          //     sigmatch_table.rows().select();
-          //   }
-          // },
-          // {
-          //     extend: 'columnToggle',
-          //     text: 'Toggle GuideToPharma / Asuka Inoue',
-          //     columns: '.primary'
-          // },
           {
               text: 'Toggle <b>GuideToPharma</b> / Asuka Inoue',
               className: 'toggle-source',
@@ -319,18 +308,12 @@ const run_sig_match = function(){
                 columns.visible(!columns.visible()[0])
               }
           },
-          // {
-          //     extend: 'columnVisibility',
-          //     text: 'Show GuideToPharma',
-          //     visibility: true,
-          //     columns: '.secondary'
-          // },
-          // {
-          //     extend: 'columnVisibility',
-          //     text: 'Hide secondary',
-          //     visibility: false,
-          //     columns: '.secondary'
-          // },
+          {
+            text: "Export to Excel",
+            action: function() {
+              tableToExcel('sigmatch_table', 'Signature Match data', 'SignatureMatch_coupling.xls')
+            }
+          },
           {
             text: 'Deselect',
             action: function () {
@@ -382,3 +365,38 @@ const filter_pairs = function() {
     .style('display', function(d){return (num <= d.pairs.length ? 'block' : 'none') })
 }
 
+var tableToExcel = (function () {
+  var uri = 'data:application/vnd.ms-excel;base64,',
+    template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
+    base64 = function (s) {
+      return window.btoa(unescape(encodeURIComponent(s)))
+    }, format = function (s, c) {
+      return s.replace(/{(\w+)}/g, function (m, p) {
+        return c[p];
+      })
+    }
+  return function (table, name, filename) {
+    var table= $("#"+table).clone();
+    $("#excel_table").html(table);
+    // Clean up table to remove yadcf stuff
+    $("#excel_table thead tr").css('height','');
+    $("#excel_table thead th").css('height','');
+    $("#excel_table thead div").css('height','');
+    $("#excel_table thead .yadcf-filter-wrapper").remove();
+    $("#excel_table thead button").remove();
+    var tr = $("#excel_table thead tr:eq(1)");
+    // reattach th titles
+    tr.find('th').each (function( column, th) {
+      if ($(th).attr('title')) $(th).html($(th).attr('title'));
+    });     
+
+    var ctx = {
+      worksheet: name || 'Worksheet',
+      table: $("#excel_table").html()
+    }
+    $("#excel_table").html("");
+    document.getElementById("dlink").href = uri + base64(format(template, ctx));
+    document.getElementById("dlink").download = filename;
+    document.getElementById("dlink").click();
+  }
+})()
