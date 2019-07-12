910d814b6ece52ecbf9fdd7da4b1d2a3f21990c6
Christian Munk <christian@munk.be>
2019-07-12 12:54:14 +0200

    fix isoforms url capture

--- protein/urls.py
+++ protein/urls.py
@@ -6,9 +6,9 @@ from protein import views
 
 urlpatterns = [
     #url(r'^$', views.BrowseSelection.as_view(), name='index'),
+    url(r'^isoforms[/]?$', views.isoforms, name='isoforms'),
     url(r'^$', cache_page(60*60*24*7)(views.BrowseSelection.as_view()), name='index'),
     url(r'^autocomplete', views.SelectionAutocomplete, name='autocomplete'),
     url(r'^gproteins', views.g_proteins, name='g_proteins'),
     url(r'^(?P<slug>[-\w]+)/$', views.detail, name='detail'),
-    url(r'^isoforms', views.isoforms, name='g_proteins'),
 ]
