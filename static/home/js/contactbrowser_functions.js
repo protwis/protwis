753bb6268c98cb6a12df638e18d053fcdd3388d0
AJK-dev <>
2019-09-24 11:32:10 +0200

    UPDATE: missing function for SVG download

--- static/home/js/contactbrowser_functions.js
+++ static/home/js/contactbrowser_functions.js
@@ -1757,3 +1757,14 @@
             $("#"+ currentTab + ' .go-button').click()
           }
         }
+
+
+        function downloadURI(uri, name) {
+            var link = document.createElement("a");
+            link.download = name;
+            link.href = uri;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            delete link;
+        }
