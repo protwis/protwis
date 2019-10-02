143c8c504bd1cc702d948a1e704ec7e8b84cc616
Christian Munk <christian@munk.be>
2019-10-02 13:54:16 +0200

    Fixed redrawing columns correctly on change of tabs Also small typos in headers

--- static/home/js/contactbrowser-tabtables.js
+++ static/home/js/contactbrowser-tabtables.js
@@ -85,6 +85,8 @@ function renderDataTablesYadcf(element) {
     // Do not re init the table.
     if ($.fn.DataTable.isDataTable(selector + " .browser-table-" + tab_number)) {
         console.timeEnd("renderDataTablesYadcf");
+        var btable = $(selector + " .browser-table-" + tab_number).DataTable();
+        btable.columns.adjust().draw();
         $(".main_loading_overlay").hide();
         return
     }
@@ -1015,6 +1017,8 @@ function renderDataTablesYadcf(element) {
     }
     // Show hidden tr now that table is rendered. (Faster rending, since less in DOM)
     table.find(".hidden").removeClass("hidden");
+    btable.columns.adjust().draw();
+
     $(".main_loading_overlay").hide();
     console.timeEnd("renderDataTablesYadcf");
 }
@@ -1094,9 +1098,9 @@ function renderBrowser(data) {
                   <tr> \
                           <th colspan="1" rowspan="2">Segment</th> \
                           <th colspan="1" rowspan="2">Positions</th> \
-                          <th colspan="3" rowspan="2"> Frequency (%)</th> \
+                          <th colspan="3" rowspan="2">Contact Frequency (%)</th> \
                           <th rowspan="2">Interactions</th> \
-                          <th rowspan="2">Distance (Ca atoms)*</th> \
+                          <th rowspan="2">Distance (Ca, Å)</th> \
                           <th colspan="4">Backbone Ca movement</th> \
                           <th colspan="2">Sidechain differences</th> \
                           <th colspan="2"></th> \
@@ -1347,9 +1351,9 @@ function renderBrowser(data) {
                   <tr> \
                           <th colspan="1" rowspan="2">Segment</th> \
                           <th colspan="1" rowspan="2">Positions</th> \
-                          <th colspan="1" rowspan="2"> Frequency (%)</th> \
+                          <th colspan="1" rowspan="2">Contact Frequency (%)</th> \
                           <th rowspan="2">Interactions</th> \
-                          <th rowspan="2">Distance (Ca atoms)*</th> \
+                          <th rowspan="2">Distance (Ca, Å)</th> \
                           <th colspan="4">Backbone Ca movement</th> \
                           <th colspan="2">Sidechain differences</th> \
                           <th colspan="2"></th> \
@@ -1557,7 +1561,7 @@ function renderBrowser(data) {
                           <th colspan="1" rowspan="2">Positions</th> \
                           <th colspan="1" rowspan="2">Positions GN</th> \
                           <th rowspan="2">Interaction</th> \
-                          <th rowspan="2">Distance (Ca atoms)*</th> \
+                          <th rowspan="2">Distance (Ca, Å)</th> \
                           <th colspan="4">Backbone Ca movement</th> \
                           <th colspan="2">Sidechain differences</th> \
                           <th colspan="2"></th> \
