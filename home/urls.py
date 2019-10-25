426a1f6073fcd8f9ae9d02641625a9ce9429b3d2
Christian Munk <christian@munk.be>
2019-10-25 14:48:01 +0200

    Updated navbar menu. Added development icon. Added things to release notes.

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,18 @@ from django.conf.urls import url
 #from home import views
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from home import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'), 
+    url(
+        r'^favicon.ico$',
+        RedirectView.as_view(
+            url=staticfiles_storage.url('home/images/favicon_dev.ico'),
+            permanent=False),
+        name="favicon"
+    )
 ]
