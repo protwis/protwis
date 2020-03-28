002908c0a297db721956c0f53f20749461ada538
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2020-03-28 08:53:29 +0100

    UPDATE: Multiple tabs with a table in each prototype ready.

--- static/home/js/multitable.js
+++ static/home/js/multitable.js
@@ -1,87 +1,60 @@
 $(document).ready(function () {
-    'use strict';
 
-    var oTable1,
-        oTable2;
+   $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      console.log( 'show tab' );
+      $($.fn.dataTable.tables(true)).DataTable()
+          .columns.adjust()
+          .responsive.recalc();
+   });
 
-    oTable1 = $('#example1').DataTable({
+   $("#familiestabletab").dataTable({
+        "scrollY": $(window).height() - 450,
+        "scrollX": true,
+        "scrollCollapse": true,
+        "scroller": true,
         "paging": false,
-        stateSave: true,
-    });
+        "bSortCellsTop": true, //prevent sort arrows going on bottom row
+        "aaSorting": [],
+        "autoWidth": true,
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+        "pageLength": -1,
+        "dom": 'Blfrtip',
+        "buttons": ['copy', 'csv', 'excel'],
+        "bInfo": true,
+   });
 
-    //----------------------------------------------
-    //notice the new yadcf API for init the filters:
-    //----------------------------------------------
+   $("#subtypestabletab").dataTable({
+      //data: data1,
+      "ordering": false,
+      "paging": false,
+      "fixedHeader":true,
+      "searching": false,
+      "info": false,
+      //"deferRender": true,
+   });
 
-    yadcf.init(oTable1, [{
-        column_number: 0
-    }, {
-        column_number: 1,
-        filter_type: "range_number_slider"
-    }, {
-        column_number: 2,
-        filter_type: "date"
-    }, {
-        column_number: 3,
-        filter_type: "auto_complete",
-        text_data_delimiter: ","
-    }, {
-        column_number: 4,
-        column_data_type: "html",
-        html_data_type: "text",
-        filter_default_label: "Select tag"
-    }]);
+   $("#bouviertabletab").dataTable({
+      //data: data1,
+      "ordering": false,
+      "paging": false,
+      "fixedHeader":true,
+      "searching": false,
+      "info": false,
+      //"deferRender": true,
+   });
 
-    oTable2 = $('#example2').DataTable({
-        "responsive": true,
-        "processing": true,
-        "ajax": "http://0.0.0.0:8000/static/sources/deep.txt",
-        "columns": [{
-            "data": "engine"
-        }, {
-            "data": "browser"
-        }, {
-            "data": "platform.inner"
-        }, {
-            "data": "platform.details.0"
-        }, {
-            "data": "platform.details.1"
-        }]
-    });
+   $("#inouetabletab").dataTable({
+      //data: data1,
+      "ordering": false,
+      "paging": false,
+      "fixedHeader":true,
+      "searching": false,
+      "info": false,
+      //"deferRender": true,
+   });
 
-    //----------------------------------------------
-    //notice the new yadcf API for init the filters:
-    //----------------------------------------------
-
-    yadcf.init(oTable2, [{
-        column_number: 0
-    }, {
-        column_number: 1,
-        filter_type: "text",
-        exclude: true,
-        exclude_label: '!(not)'
-    }, {
-        column_number: 2,
-        filter_type: "auto_complete"
-    }, {
-        column_number: 3,
-        filter_type: "range_number_slider",
-        ignore_char: "-"
-    }, {
-        column_number: 4
-    }]);
-
-    // By default display the first tab. If this is not on one has to click on the tab for display.
-    $('#myTab a:first').tab('show');
-
-//    yadcf.exFilterColumn(oTable2, [[0, "Misc"]]);
-
-
-    yadcf.initMultipleTables([oTable1, oTable2], [{
-        filter_container_id: 'multi-table-filter',
-        filter_default_label: 'Filter all tables!'
-    }]);
+   // By default display the first tab. If this is not ON, one has to click on the tab for display.
+   $('#myTab a:first').tab('show');
 
 });
-
 
