cfb80072060d2da43584a2a3a4f8105ff723c85b
AlibekMamyrbekov <31478442+AlibekMamyrbekov@users.noreply.github.com>
2020-06-16 16:01:52 +0600

    shading updates

--- static/home/js/shading.js
+++ static/home/js/shading.js
@@ -1,58 +1,69 @@
 class MyShadingTable {
 
 	constructor(table, columns, darkGrey, lightGrey) {
-        this.darkGrey = darkGrey
-		this.lightGrey = lightGrey
-        this.table = table
-        this.minValues = []
-        this.maxValues = []
-        this.columnsIndexes = new Set(columns.filter(word => typeof word === 'number'))
+        this.darkGrey = darkGrey;
+		this.lightGrey = lightGrey;
+        this.table = table;
+        this.minValues = [];
+        this.maxValues = [];
+        this.setColumns(columns);
+    }
 
-        let columnsStrings = columns.filter(word => this.isValidSelector(word))
-        let columnsClasses = columnsStrings.filter(word => word[0] == '.')
-        let columnsIds = columnsStrings.filter(word => word[0] == '#')
-        let that = this
+    setColumns(columns) {
+        console.log(columns)
+        this.columnsIndexes = new Set(columns.filter(word => typeof word === "number"));
+        let columnsStrings = columns.filter(word => this.isValidSelector(word));
+        console.log('name2 '+columnsStrings)
+        this.columnsClasses = columnsStrings.filter(word => word[0] == ".");
+        this.columnsIds = columnsStrings.filter(word => word[0] == "#");
+    }
 
-        for (let i = 0; i < columnsIds.length; i++) {
-            this.table.column(columnsIds[i]).every(function() {
-                that.columnsIndexes.add(this.index() + 1)
+    getIndexes() {
+        let indexes = new Set(this.columnsIndexes);
+
+        for (let i = 0; i < this.columnsIds.length; i++) {
+            this.table.column(this.columnsIds[i]).every(function() {
+
+                indexes.add(this.index() + 1);
             });
         }
 
-        for (let i = 0; i < columnsClasses.length; i++) {
-            this.table.columns(columnsClasses[i]).every(function() {
-                that.columnsIndexes.add(this.index() + 1)
+        for (let i = 0; i < this.columnsClasses.length; i++) {
+            this.table.columns(this.columnsClasses[i]).every(function() {
+
+                indexes.add(this.index() + 1);
+                console.log(indexes)
             });
         }
+
+        return indexes;
     }
 
     calculateLimitValues(data) {
-        if (this.columnsIndexes.size <= 0)
-            return
-
-        this.maximumColumn = Math.max.apply(null, [...this.columnsIndexes])
+        let columnsLength = this.table.columns().nodes().length;
 
         // minValues and maxValues will have the minimum and maximum
         // values for the selected columns, and null for the rest.
-        this.minValues = new Array(this.maximumColumn + 1).fill(null);
-        this.maxValues = new Array(this.maximumColumn + 1).fill(null);
+        this.minValues = new Array(columnsLength + 1).fill(null);
+        this.maxValues = new Array(columnsLength + 1).fill(null);
 
-        for (let n of this.columnsIndexes) {
-            this.minValues[n] = this.getMin(n, data)
-            this.maxValues[n] = this.getMax(n, data)
+        let indexes = this.getIndexes();
+        for (let n of indexes) {
+            this.minValues[n] = this.getMin(n, data);
+            this.maxValues[n] = this.getMax(n, data);
         }
     }
 
     shade(rows) {
-        let that = this
-        this.calculateLimitValues(rows.data())
+        let that = this;
+        this.calculateLimitValues(rows.data());
         rows.every(function(){
-            that.shadeMyTableRow(this.node(), this.data())
+            that.shadeMyTableRow(this.node(), this.data());
         });
 	}
 
     isValidSelector(selector) {
-        if (typeof(selector) !== 'string')
+        if (typeof(selector) !== "string")
             return false;
 
         try {
@@ -69,19 +80,19 @@ class MyShadingTable {
             return null;
 
         // This handles nested font elements properly.
-        if ($(text).is('font'))
-            text = $(text).text()
+        if ($(text).is("font"))
+            text = $(text).text();
 
-        if (text == null || text == '' || isNaN(text))
-            return null
+        if (text == null || text == "" || isNaN(text))
+            return null;
         else
-            return text
+            return text;
     }
 
     getMax(numberColumn, data) {
-        let that = this
+        let that = this;
         return data.reduce(function(total, currentValue, currentIndex, arr) {
-            let value = that.getValidNumber(currentValue[numberColumn-1], currentIndex)
+            let value = that.getValidNumber(currentValue[numberColumn-1], currentIndex);
             if (value)
                 return Math.max(total, value);
             else
@@ -90,9 +101,9 @@ class MyShadingTable {
 	}
 
 	getMin(numberColumn, data) {
-        let that = this
+        let that = this;
         return data.reduce(function(total, currentValue, currentIndex, arr) {
-            let value = that.getValidNumber(currentValue[numberColumn-1], currentIndex)
+            let value = that.getValidNumber(currentValue[numberColumn-1], currentIndex);
             if (value)
                 return Math.min(total, value);
             else
@@ -100,40 +111,50 @@ class MyShadingTable {
 		}, Number.POSITIVE_INFINITY);
 	}
 
-	shadeMyTableRow(row, data, index){
+	shadeMyTableRow(row, data, index) {
         for (let i = 0; i < this.minValues.length; i++)
             if (this.minValues[i] != null)
-                this.paintCellOfColumn(i, this.minValues[i], this.maxValues[i], row, data)
+                this.paintCellOfColumn(i, this.minValues[i], this.maxValues[i], row, data);
+            else
+                this.paintCellOfColumn(i, 0, 0, row, data);
 	}
 
-	paintCellOfColumn(numberColumn, min, max, row, data){
+	paintCellOfColumn(numberColumn, min, max, row, data) {
         if (max <= min) {
-            $(row).find('td:eq(' + (numberColumn-1) + ')').css('background',
-                        'none');
+            $(row).find("td:eq(" + (numberColumn-1) + ")").css("background",
+                        "none");
             return;
         }
 
-        let value = this.getValidNumber(data[numberColumn-1], 0)
+        let value = this.getValidNumber(data[numberColumn-1], 0);
         if (!value)
             return;
 
-		let factor = (value - min) / (max - min)
-		let color = this.lightGrey - factor * (this.lightGrey - this.darkGrey)
-        let n = Math.round(color)
+		let factor = (value - min) / (max - min);
+		let color = this.lightGrey - factor * (this.lightGrey - this.darkGrey);
+        let n = Math.round(color);
 
-        $(row).find('td:eq(' + (numberColumn-1) + ')').css('background',
-                    'rgb(' + n + ',' + n + ',' + n + ')');
+        $(row).find("td:eq(" + (numberColumn-1) + ")").css("background",
+                    "rgb(" + n + "," + n + "," + n + ")");
 	}
 }
 
+// - The first argument is the table instance.
+// - The second are the columns, e.g. [25, 26, ".niceColumn", "#firstColumn"].
+// - The fourth and the fifth are the darkest and lightest grey colors:
+//   valid values are from 0 to 255.
 function shadeTable(table, columnIndexes, darkGrey, lightGrey)
-{
-    let myShadingTable = new MyShadingTable(table, columnIndexes, darkGrey, lightGrey);
+{   let myShadingTable = null;
+     myShadingTable = new MyShadingTable(table, columnIndexes, darkGrey, lightGrey);
 
-    table.on('draw.dt', function(e, settings) {
-        let api = new $.fn.dataTable.Api(settings)
-        myShadingTable.shade(api.rows({page: 'current'}))
-    });
+    // table.on("draw.dt", function(e, settings) {
+    //     let api = new $.fn.dataTable.Api(settings);
+    //     myShadingTable.shade(api.rows({page: "current"}));
+    // });
 
-    myShadingTable.shade(table.rows({page: 'current'}))
+    myShadingTable.shade(table.rows({page: "current"}));
 }
+
+// Uncomment the following 2 lines to test index.html:
+// let table = $("#structures_test").DataTable();
+// shadeTable(table, ["#firstColumn", ".niceColumn", 2], 80, 200);
