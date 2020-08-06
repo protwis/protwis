9aa5bc3991dc0c164632c57785738284b3a2fb75
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2020-08-06 17:15:17 +0200

    UPDATE: Minor changes. Link to stats in main page, clean html. - Link to statistics page in index page. - Change blue border to rgba with transparent property - Cleanup odd all-embedding in html row. - Refactor views in the *pages* app to Django 3. Just a reminder that the *url* function is in a path to deprecation. Still works as it has been made into an alias of django.urls.re_path(), but better to use ```from django.urls import path.```

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 
-from pages import views
+from . import views
 
 urlpatterns = [
-	url(r'^releasenotes', views.releasenotes, name='releasenotes'), 
+    path('releasenotes/', views.releasenotes, name='releasenotes'),
 ]
