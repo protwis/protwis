df3b4aea4db3f7d59a5bd7c6f32e49b623817299
AJK-dev <15200114+AJK-dev@users.noreply.github.com>
2021-02-28 11:35:14 +0100

    CODACY: code cleaning

--- common/middleware/stats.py
+++ common/middleware/stats.py
@@ -2,8 +2,11 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.db import connection
 
-import time, datetime, os
+import time
+import datetime
+import os
 #import uuid
+
 
 class StatsMiddleware:
     def __init__(self, get_response):
@@ -11,7 +14,7 @@ class StatsMiddleware:
         # One-time configuration and initialization.
 
     def __call__(self, request):
-        '''Handling protwis request logs.'''
+        """Handling protwis request logs."""
 #       # Start/top logger - not needed as we do slow query logging instead
 #        request_id = uuid.uuid4().hex
 #        text_file = open(os.path.join(settings.BASE_DIR, "logs/stats_start_stop.log"), "a")
@@ -24,13 +27,13 @@ class StatsMiddleware:
         # Handle request
         response = self.get_response(request)
 
-        # Code below is executed after handling the request
-
+        # CODE BELOW is executed after handling the request
         # End timer
         total = time.time() - start_time
 
         if settings.DEBUG:
-            print(request.path,"Time to execute", round(total,2), "SQL queries",len(connection.queries))
+            print(request.path, "Time to execute", round(
+                total, 2), "SQL queries", len(connection.queries))
 
         bot_user = self.bot_detection(request)
         log_file = os.path.join(settings.BASE_DIR, "logs/stats.log")
@@ -39,9 +42,11 @@ class StatsMiddleware:
 
         text_file = open(log_file, "a")
         if bot_user:
-            text_file.write('%s %s %s %s %s %s\n' % (datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), round(total,2), request.META.get('HTTP_X_FORWARDED_FOR'), request.method, request.path, request.META.get('HTTP_USER_AGENT') ))
+            text_file.write('%s %s %s %s %s %s\n' % (datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), round(total, 2),
+                                                     request.META.get('HTTP_X_FORWARDED_FOR'), request.method, request.path, request.META.get('HTTP_USER_AGENT')))
         else:
-            text_file.write('%s %s %s %s %s\n' % (datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), round(total,2), request.META.get('HTTP_X_FORWARDED_FOR'), request.method, request.path))
+            text_file.write('%s %s %s %s %s\n' % (datetime.datetime.utcnow().strftime(
+                "%Y-%m-%d %H:%M:%S"), round(total, 2), request.META.get('HTTP_X_FORWARDED_FOR'), request.method, request.path))
 
         text_file.close()
 
@@ -51,12 +56,14 @@ class StatsMiddleware:
 #        text_file.close()
 
         # Extended logging of queries that are slower than 5 seconds
-        if total>5:
+        if total > 5:
             log_file = os.path.join(settings.BASE_DIR, "logs/stats_slow.log")
             if bot_user:
-                log_file = os.path.join(settings.BASE_DIR, "logs/stats_slow_bots.log")
+                log_file = os.path.join(
+                    settings.BASE_DIR, "logs/stats_slow_bots.log")
             text_file = open(log_file, "a")
-            text_file.write('%s %s %s %s %s %s\n' % (datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), round(total,2), request.META.get('HTTP_X_FORWARDED_FOR'), request.method, request.path, request.META.get('HTTP_USER_AGENT') ))
+            text_file.write('%s %s %s %s %s %s\n' % (datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), round(total, 2),
+                                                     request.META.get('HTTP_X_FORWARDED_FOR'), request.method, request.path, request.META.get('HTTP_USER_AGENT')))
             text_file.close()
 
         return response
@@ -67,17 +74,19 @@ class StatsMiddleware:
             log_file = os.path.join(settings.BASE_DIR, "logs/errors_bots.log")
 
         text_file = open(log_file, "a")
-        text_file.write('%s %s %s %s - %s %s - %s "%s" "%s"\n' % (datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), request.method, request.path, request.META.get('HTTP_REFERER'), request.META.get('HTTP_X_FORWARDED_FOR'), request.META.get('HTTP_USER_AGENT'), str(exception) ))
+        text_file.write('%s %s - %s %s - %s "%s" "%s"\n' % (datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), request.method, request.path,
+                                                            request.META.get('HTTP_REFERER'), request.META.get('HTTP_X_FORWARDED_FOR'), request.META.get('HTTP_USER_AGENT'), str(exception)))
         text_file.close()
 
     @staticmethod
     def bot_detection(request):
-        '''Simplistic bot detection to split log files.
+        """
+        Simplistic bot detection to split log files.
 
         This function matches the user agent string for specific words matching
         all main search engine agents and >60% of any bot agents.
         Analyzed Feb-2021 on listings at useragentstring.com.
-        '''
+        """
         bot_IDs = ["bot", "slurp", "crawler", "spider", "curl"]
         user_agent = request.META.get("HTTP_USER_AGENT", "").lower()
         return any(bot_ID in user_agent for bot_ID in bot_IDs)
