bf1a2570f63934aed0b4e7f9020567ead3364762
Mojito88 <jimmycaroli88@gmail.com>
2021-04-09 15:59:55 +0200

    CODACY: fixing var names and unused call

--- ligand/views.py
+++ ligand/views.py
@@ -1,7 +1,7 @@
 import json
 import itertools
 from copy import deepcopy
-from collections import defaultdict, OrderedDict, Counter
+from collections import defaultdict, OrderedDict
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView, DetailView, ListView
@@ -641,14 +641,14 @@ class LigandBiasStatistics(TemplateView):
             "family", "experiment__receptor__entry_name", "experiment__ligand")
 
         circles = {}
-        for tuple in assay_qs:
-            if tuple[1].split('_')[1] == 'human':
-                key = tuple[1].split('_')[0].upper()
+        for data in assay_qs:
+            if data[1].split('_')[1] == 'human':
+                key = data[1].split('_')[0].upper()
                 if key not in circles.keys():
                     circles[key] = {}
-                    circles[key][tuple[0]] = 0
+                    circles[key][data[0]] = 0
                 else:
-                    circles[key][tuple[0]] = 0
+                    circles[key][data[0]] = 0
 
         for data in ligand_qs:
             if data[1].split('_')[1] == 'human':
