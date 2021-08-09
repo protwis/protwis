2ebd66c5bd64d77181f66b0f7afe9a8515db7e78
AlibekMamyrbekov <31478442+AlibekMamyrbekov@users.noreply.github.com>
2021-08-09 11:30:15 +0200

    NEW: fetch endogenous ligands from GTPO

--- tools/management/commands/get_gtp_ligands.py
+++ tools/management/commands/get_gtp_ligands.py
@@ -1,14 +1,11 @@
-from concurrent.futures import ProcessPoolExecutor
-from multiprocessing import Pool
 from django.db import IntegrityError
 from build.management.commands.base_build import Command as BaseBuild
 from protein.models import Protein
-from ligand.models import *
+from ligand.models import GTP_endogenous_ligand, Ligand,
 from common.models import WebLink, WebResource, Publication
 import logging
 import time
 import requests
-from multiprocessing.dummy import Pool as ThreadPool
 
 MISSING_PROTEINS = {}
 SKIPPED = 0
@@ -296,7 +293,7 @@ class Command(BaseBuild):
         return pub
 
     def get_publication(self, publication):
-        return Publication.objects.filter(web_link = web_link)[0]
+        return Publication.objects.filter(Web_link = web_link)[0]
 
     def get_ligands(self):
         response = requests.get(
@@ -334,8 +331,7 @@ class Command(BaseBuild):
                     if entry['parentFamilyIds'][0] == 694 or entry['parentFamilyIds'][0] == 115:
                         target_list.extend(entry['targetIds'])
             except:
-                print("Was a nice sleep, now let me continue...")
-                pass
+                print("Was a nice sleep, now let me continue...")                
         return target_list
 
     def get_ligand_assays(self, targets):
