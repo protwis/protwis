c268e17cb34de7adae79c0bd107a608d74ce09a0
Gáspár Pándy-Szekeres <gaspar@sund.ku.dk>
2021-09-03 10:31:05 +0200

    FIX: set build_structures paralelization at 4 max cores

--- build/management/commands/build_all.py
+++ build/management/commands/build_all.py
@@ -35,6 +35,11 @@ class Command(BaseCommand):
         if options['test']:
             print('Running in test mode')
 
+        if options['proc']>4:
+            safe_proc_num = 4
+        else:
+            safe_proc_num = options['proc']
+
         phase1 = [
             ['build_common'],
             ['build_citations'],
@@ -45,7 +50,7 @@ class Command(BaseCommand):
             ['build_blast_database'],
             ['build_links'],
             ['build_construct_proteins'],
-            ['build_structures', {'proc': options['proc']}],
+            ['build_structures', {'proc': safe_proc_num],
             ['build_endogenous_ligands'],
             ['build_consensus_sequences', {'proc': options['proc']}],
             ['build_g_proteins'],
