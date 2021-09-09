870b9be6901ea355a789c1e8829c02dccc448135
AJK-dev <15200114+AJK-dev@users.noreply.github.com>
2021-09-09 17:07:25 +0200

    FIX: Sitesearch - target selection, PDB submit, limit BLAST DBs to GPCRs

--- build/management/commands/build_blast_database.py
+++ build/management/commands/build_blast_database.py
@@ -23,12 +23,12 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        # BLAST database all sequences
-        proteins = Protein.objects.filter(sequence_type__slug="wt")
+        # BLAST database all GPCR sequences
+        proteins = Protein.objects.filter(sequence_type__slug="wt", family__slug__startswith="00")
         self.build_database(proteins, "protwis_blastdb")
 
-        # BLAST database only human sequences
-        proteins = Protein.objects.filter(sequence_type__slug="wt", species__common_name="Human")
+        # BLAST database only human GPCR sequences
+        proteins = Protein.objects.filter(sequence_type__slug="wt", species__common_name="Human", family__slug__startswith="00")
         self.build_database(proteins, "protwis_human_blastdb")
 
         # BLAST database only human GPCR sequences for 7TM bundle
