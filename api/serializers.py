d4e9a138d8ac5ba27d570cd952d091bcca2b1dc2
Gáspár Pándy-Szekeres <gaspar@sund.ku.dk>
2022-11-14 12:20:13 +0100

    UPDATE: added interaction count, renamed some fields

--- api/serializers.py
+++ api/serializers.py
@@ -124,16 +124,24 @@ class StructurePeptideLigandInteractionSerializer(serializers.ModelSerializer):
     ligand_chain = serializers.ReadOnlyField(source='interacting_peptide_pair.peptide.chain')
     peptide_amino_acid = serializers.ReadOnlyField(source='interacting_peptide_pair.peptide_amino_acid')
     peptide_amino_acid_three_letter = serializers.ReadOnlyField(source='interacting_peptide_pair.peptide_amino_acid_three_letter')
-    peptide_sequence_number = serializers.ReadOnlyField(source='interacting_peptide_pair.peptide_sequence_number')
-    amino_acid = serializers.ReadOnlyField(source='interacting_peptide_pair.receptor_residue.amino_acid')
-    sequence_number = serializers.ReadOnlyField(source='interacting_peptide_pair.receptor_residue.sequence_number')
+    peptide_residue_number = serializers.ReadOnlyField(source='interacting_peptide_pair.peptide_sequence_number')
+    receptor_amino_acid = serializers.ReadOnlyField(source='interacting_peptide_pair.receptor_residue.amino_acid')
+    receptor_residue_number = serializers.ReadOnlyField(source='interacting_peptide_pair.receptor_residue.sequence_number')
     display_generic_number = serializers.ReadOnlyField(source='interacting_peptide_pair.receptor_residue.display_generic_number.label')
     interaction_type = serializers.ReadOnlyField()
     interaction_level = serializers.ReadOnlyField()
+    interaction_count = serializers.SerializerMethodField()
+
+    def get_interaction_count(self, obj):
+        return InteractionPeptide.objects.filter(interacting_peptide_pair__peptide__structure__pdb_code__index=obj.interacting_peptide_pair.peptide.structure.pdb_code.index, 
+                                                 interacting_peptide_pair__peptide_sequence_number=obj.interacting_peptide_pair.peptide_sequence_number,
+                                                 interacting_peptide_pair__receptor_residue__sequence_number=obj.interacting_peptide_pair.receptor_residue.sequence_number,
+                                                 interaction_type=obj.interaction_type).count()
+
     class Meta:
         model = InteractionPeptide
-        fields = ('pdb_code', 'ligand_name', 'ligand_chain', 'peptide_amino_acid', 'peptide_amino_acid_three_letter', 'peptide_sequence_number',
-                  'amino_acid', 'sequence_number', 'display_generic_number', 'interaction_type', 'interaction_level')
+        fields = ('pdb_code', 'ligand_name', 'ligand_chain', 'peptide_amino_acid', 'peptide_amino_acid_three_letter', 'peptide_residue_number',
+                  'receptor_amino_acid', 'receptor_residue_number', 'display_generic_number', 'interaction_type', 'interaction_level', 'interaction_count')
 
 
 class MutationSerializer(serializers.ModelSerializer):
