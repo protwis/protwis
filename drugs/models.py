4d50dab80edede352e2088ec254b00b187c1a4b3
Jimmy-A-Caroli <jimmycaroli88@gmail.com>
2024-02-07 13:39:34 +0100

    UPDATE: setting up new models (no migrations yet)

--- drugs/models.py
+++ drugs/models.py
@@ -22,3 +22,37 @@ class Drugs(models.Model):
 
     class Meta():
         db_table = 'drugs'
+
+
+class Drugs2024(models.Model):
+    target = models.ForeignKey('protein.Protein', on_delete=models.CASCADE)
+    indication = models.ForeignKey('Indication', on_delete=models.CASCADE)
+    ligand = models.ForeignKey('ligand.Ligand', on_delete=models.CASCADE)
+    charge = models.CharField(max_length=2)
+    complexity = models.CharField(max_length=10)
+    tpsa = models.CharField(max_length=10)
+    drug_status = models.CharField(max_length=15)
+    approval_year = models.CharField(max_length=5)
+    indication_max_phase = models.CharField(max_length=1)
+    moa = models.CharField(max_length=30, null=True)
+    affected_pathway = models.CharField(max_length=30)
+    somatic_mutation = models.CharField(max_length=30)
+    similarity_to_model = models.CharField(max_length=30)
+    novelty_score = models.CharField(max_length=5)
+    publication = models.ManyToManyField('common.Publication')
+
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        db_table = 'drugs'
+
+class Indication(models.Model):
+    name =  models.CharField(max_length=55)
+    code =  models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        db_table = 'indication'
