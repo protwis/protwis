b363c1f75469657095b9b0eba9a33ddb866722c1
Jimmy-A-Caroli <jimmycaroli88@gmail.com>
2024-02-08 10:04:44 +0100

    UPDATE: final model setup plus lots of migrations

--- drugs/models.py
+++ drugs/models.py
@@ -29,23 +29,23 @@ class Drugs2024(models.Model):
     indication = models.ForeignKey('Indication', on_delete=models.CASCADE)
     ligand = models.ForeignKey('ligand.Ligand', on_delete=models.CASCADE)
     charge = models.CharField(max_length=2)
-    complexity = models.CharField(max_length=10)
+    complexity = models.FloatField(max_length=4)
     tpsa = models.CharField(max_length=10)
     drug_status = models.CharField(max_length=15)
-    approval_year = models.CharField(max_length=5)
-    indication_max_phase = models.CharField(max_length=1)
+    approval_year = models.IntegerField(max_length=4)
+    indication_max_phase = models.IntegerField(max_length=1)
     moa = models.CharField(max_length=30, null=True)
     affected_pathway = models.CharField(max_length=30)
     somatic_mutation = models.CharField(max_length=30)
-    similarity_to_model = models.CharField(max_length=30)
-    novelty_score = models.CharField(max_length=5)
-    publication = models.ManyToManyField('common.Publication')
+    similarity_to_model = models.FloatField(max_length=4)
+    novelty_score = models.FloatField(max_length=4)
+    reference = models.ManyToManyField('common.Publication')
 
     def __str__(self):
         return self.name
 
     class Meta():
-        db_table = 'drugs'
+        db_table = 'drugs_new'
 
 class Indication(models.Model):
     name =  models.CharField(max_length=55)
