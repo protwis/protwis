fe21345d2542fb798a439464611b1c7cf6b6bb7e
Jimmy-A-Caroli <jimmycaroli88@gmail.com>
2024-05-02 15:58:54 +0200

    UPDATE: setting up the new Data Mapper environment

---
+++ mapper/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.urls import path
+from django.views.decorators.cache import cache_page
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.LandingPage.as_view(), name='landing_page'),
+]
---
+++ mapper/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views import generic
+from django.http import JsonResponse, HttpResponse
+from django.db.models import Q, F, Func, Value, Prefetch
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+from django.urls import reverse
+from django.shortcuts import render
+from django.conf import settings
+from django.views.decorators.cache import cache_page
+from django.views.generic import TemplateView
+
+
+from protein.models import Protein, ProteinConformation, ProteinAlias, ProteinFamily, Gene, ProteinSegment
+from residue.models import Residue
+from structure.models import Structure, StructureModel, StructureExtraProteins
+from interaction.models import ResidueFragmentInteraction,StructureLigandInteraction
+from common.selection import Selection
+from common.views import AbsBrowseSelection
+from ligand.models import Ligand, LigandID
+
+import json
+from copy import deepcopy
+from collections import OrderedDict
+
+
+class LandingPage(TemplateView):
+    template_name = 'mapper/data_mapper_landing.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+#
+# def LandingPage(request):
+#     return render(request, 'mapper/data_mapper_landing.html')
