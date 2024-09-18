bd93eacbf732fe5f39371d82ff0c5b6d4e693bcf
ismaelresp <7013638+ismaelresp@users.noreply.github.com>
2024-09-18 19:54:09 +0200

    CODACY FIX

--- protein/model_func.py
+++ protein/model_func.py
@@ -1,25 +1,25 @@
 from protein.models import ProteinFamily
 
-""" This is a solution for new builds with change of the definition of CLASSLESS_PARENT_GPCR_SLUGS
-
-    old_slug_dict:  a dict with the deprecated classless parent GPCR family slugs. 
-                    Format: {CLASSLESS_PARENT_GPCR_SLUG:CLASSLESS_PARENT_GPCR_NAME}
-
-    new_slug_set:   The new CLASSLESS_PARENT_GPCR_SLUGS constant value. A set that contains the new
-                    classless parent GPCR family slugs.
-
-    Example:    from protein.model_func import get_current_classless_parent_gpcr_slugs
-                CLASSLESS_PARENT_GPCR_SLUGS = get_current_classless_parent_gpcr_slugs({'008':'Other GPCRs'}, {'010'})
-  
-    If slugs and family names in old_slug_dict do not match with the ones in the database or they are 
-    missing in the database, returns new_slug_set. It also returns new_slug_set if a slug of new_slug_set 
-    not present in old_slug_dict is present in the database. Otherwise, returns a set that contains
-    the keys of old_slug_dict.
-
-    The function must be imported and run in protein.models after class ProteinFamily is defined.
-
- """
 def get_current_classless_parent_gpcr_slugs(old_slug_dict, new_slug_set):
+    """ This is a solution for new builds with change of the definition of CLASSLESS_PARENT_GPCR_SLUGS
+
+        old_slug_dict:  a dict with the deprecated classless parent GPCR family slugs. 
+                        Format: {CLASSLESS_PARENT_GPCR_SLUG:CLASSLESS_PARENT_GPCR_NAME}
+
+        new_slug_set:   The new CLASSLESS_PARENT_GPCR_SLUGS constant value. A set that contains the new
+                        classless parent GPCR family slugs.
+
+        Example:    from protein.model_func import get_current_classless_parent_gpcr_slugs
+                    CLASSLESS_PARENT_GPCR_SLUGS = get_current_classless_parent_gpcr_slugs({'008':'Other GPCRs'}, {'010'})
+    
+        If slugs and family names in old_slug_dict do not match with the ones in the database or they are 
+        missing in the database, returns new_slug_set. It also returns new_slug_set if a slug of new_slug_set 
+        not present in old_slug_dict is present in the database. Otherwise, returns a set that contains
+        the keys of old_slug_dict.
+
+        The function must be imported and run in protein.models after class ProteinFamily is defined.
+
+    """
     old_slug_dict_keys = old_slug_dict.keys()
     old_slug_dict_keys_set = set(old_slug_dict_keys)
 
