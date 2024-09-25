1107cfef39be10265c07c958beb63724de8ba8d3
ismaelresp <7013638+ismaelresp@users.noreply.github.com>
2024-09-25 19:57:59 +0200

    UPDATE: added settings_local_development for WSL + Docker PostgreSQL

---
+++ protwis/settings_local_development_wsl.py
@@ -0,0 +1,19 @@
+﻿# Local settings for django server running in Microsoft Windows 
+# WSL + Docker with PostgreSQL.
+# Copy and edit the contents of this file to protwis/settings_local.py
+import socket
+
+# importing defaults
+from protwis.settings_local_development import *
+
+# Database. f-string requires Python 3.6+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'protwis',
+        'USER': 'protwis',
+        'PASSWORD': 'protwis',
+        'HOST': f'{socket.gethostname()}.local',
+    }
+}
+
