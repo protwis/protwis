ca6cc07e6f942d33bd9aa89e80cff3086f43089b
ismaelresp <7013638+ismaelresp@users.noreply.github.com>
2024-09-30 13:41:55 +0200

    CODACY FIX

--- protwis/settings_local_development_wsl.py
+++ protwis/settings_local_development_wsl.py
@@ -5,6 +5,7 @@ import socket
 
 # importing defaults
 from protwis.settings_local_development import *
+from protwis.settings_local_development import DATABASES
 
 # Database. f-string requires Python 3.6+
 DATABASES['default']['HOST'] = f'{socket.gethostname()}.local'
