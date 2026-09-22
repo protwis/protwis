1045cb36e05d3408a83810a06dbad9319eb7d55e
AidanJFlynn <aidan.ja.flynn@gmail.com>
2026-09-22 15:47:35 +0200

    Add caching to structure coverage chart data

--- structure/views.py
+++ structure/views.py
@@ -1924,7 +1924,11 @@ class StructureStatistics(TemplateView):
 
             #GPCR year-to-year structure data for chart
             context['family_hierarchy'] = self.get_structure_family_hierarchy(gpcr_structure_queries)
-            context['chart_data'] = self.prepare_chart_data(gpcr_structure_queries, lookup)
+            chart_data = cache.get('structure_stats_yearly_chart_data')
+            if not chart_data:
+                chart_data = self.prepare_chart_data(gpcr_structure_queries, lookup)
+                cache.set('structure_stats_yearly_chart_data', chart_data, None)  #Non-expiring cache, as this data is not expected to change often and is expensive to compute
+            context['chart_data'] = chart_data
 
             group_cols = ["state_id__slug", "protein_conformation__protein__parent__entry_name", "pdb_code_id__index"]
             circle_data = gpcr_structure_queries['redundant']['complexed_or_not']['all_states'] \
